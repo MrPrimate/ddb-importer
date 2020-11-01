5e89409ad41456d78679b84e329a0922698f7f7f
Jack Holloway <jack@mrprimate.co.uk>
2020-11-01 22:36:25 +0000

    Monster endpoint

--- src/muncher/monster.js
+++ src/muncher/monster.js
@@ -23,6 +23,31 @@ export default class MonsterMuncher extends Application {
     this.close();
   }
 
+  static getMonsterData(searchTerm) {
+    const cobaltCookie = game.settings.get("ddb-importer", "cobalt-cookie");
+    const parsingApi = game.settings.get("ddb-importer", "api-endpoint");
+    const body = { cobalt: cobaltCookie };
+    // const body = {};
+    return new Promise((resolve, reject) => {
+      fetch(`${parsingApi}/getMonsters`, {
+      // fetch(`${parsingApi}/getMonsters/${searchTerm}`, {
+        method: "POST",
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(body), // body data type must match "Content-Type" header
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    });
+  }
+
   static async parseCritter(monsterSearchName, updateBool) {
     // const parsingApi = game.settings.get("ddb-importer", "api-endpoint");
     console.log(`munching monsters! ${monsterSearchName} ${updateBool}`); // eslint-disable-line no-console
