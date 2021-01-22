3e0b8ea5a3539488444daa535aa4777d97ed4007
Jack Holloway <jack@mrprimate.co.uk>
2021-01-22 23:55:04 +0000

    Syncing split

--- src/character/sync.js
+++ src/character/sync.js
@@ -1,6 +1,6 @@
-import utils from "../utils.js";
 import logger from "../logger.js";
 import { getCharacterData } from "./import.js";
+import { isEqual } from "../../vendor/isequal.js";
 
 async function updateCharacterCall(characterId, path, bodyContent) {
   const cobaltCookie = game.settings.get("ddb-importer", "cobalt-cookie");
@@ -37,57 +37,140 @@ async function updateCharacterCall(characterId, path, bodyContent) {
   });
 }
 
+async function spellSlots(actor, characterId, ddbData) {
+  let results = [];
+  if (actor.data.data.spells.pact.max > 0 &&
+    ddbData.character.character.data.spells.pact.value !== actor.data.data.spells.pact.value
+  ) {
+    const used = actor.data.data.spells.pact.max - actor.data.data.spells.pact.value;
+    let spellSlotPackData = {
+      spellslots: {},
+      pact: true,
+    };
+    spellSlotPackData.spellslots[`level${actor.data.data.spells.pact.level}`] = used;
+    const spellPactSlots = updateCharacterCall(characterId, "spell/slots", spellSlotPackData);
+    results.push(spellPactSlots);
+  }
+
+  let spellSlotData = { spellslots: {}, update: false };
+  for (let i = 1; i <= 9; i++) {
+    let spellData = actor.data.data.spells[`spell${i}`];
+    if (spellData.max > 0 && ddbData.character.character.data.spells[`spell${i}`].value !== spellData.value) {
+      const used = spellData.max - spellData.value;
+      spellSlotData.spellSlots[`spell${i}`] = used;
+      spellSlotData['update'] = true;
+    }
+  }
+  if (spellSlotData['update']) {
+    const spellSlots = updateCharacterCall(characterId, "spells/slots", spellSlotData);
+    results.push(spellSlots);
+  }
+
+
+  return results;
+}
+
+async function currency(actor, characterId, ddbData) {
+  let result = [];
+  const same = isEqual(ddbData.character.character.data.currency, actor.data.data.currency);
+
+  if (!same) {
+    result.push(updateCharacterCall(characterId, "currency", actor.data.data.currency));
+  };
+
+  return result;
+}
+
+async function hitPoints(actor, characterId, ddbData) {
+  let result = [];
+  localTemp = (actor.data.data.attributes.hp.temp) ? actor.data.data.attributes.hp.temp : 0;
+  const same = ddbData.character.character.data.attributes.hp.value === actor.data.data.attributes.hp.value &&
+    ddbData.character.character.data.attributes.hp.temp === localTemp;
+
+  if (!same) {
+    const hitPointData = {
+      removedHitPoints: actor.data.data.attributes.hp.max - actor.data.data.attributes.hp.value,
+      temporaryHitPoints: localTemp,
+    };
+    const hitPoints = updateCharacterCall(characterId, "hitpoints", hitPointData);
+    result.push(hitPoints);
+  };
+
+  return result;
+}
+
+async function inspiration(actor, characterId, ddbData) {
+  let result = [];
+  const same = ddbData.character.character.data.attributes.inspiration === actor.data.data.attributes.inspiration;
+
+  if (!same) {
+    const inspiration = updateCharacterCall(characterId, "inspiration", {
+      inspiration: actor.data.data.attributes.inspiration,
+    });
+    result.push(inspiration);
+  };
+
+  return result;
+}
+
+async function exhaustion(actor, characterId, ddbData) {
+  let result = [];
+  const same = ddbData.character.character.data.exhaustion === actor.data.data.attributes.exhaustion;
+
+  if (!same) {
+    let exhaustionData = {
+      conditionId: 4,
+      addCondition: false,
+    };
+    if (actor.data.data.attributes.exhaustion !== 0) {
+      exhaustionData["level"] = actor.data.data.attributes.exhaustion;
+      exhaustionData["totalHP"] = actor.data.data.attributes.hp.max;
+      exhaustionData["addCondition"] = true;
+    }
+    const ex = updateCharacterCall(characterId, "condition", exhaustionData);
+    result.push(ex);
+  };
+
+  return result;
+}
+
+async function deathSaves(actor, characterId, ddbData) {
+  let result = [];
+  const same = isEqual(ddbData.character.character.data.attributes.death, actor.data.data.attributes.death);
+
+  if (!same) {
+    const deathSaveData = {
+      failCount: actor.data.data.attributes.death.failure,
+      successCount: actor.data.data.attributes.death.success,
+    };
+    const deathSaves = updateCharacterCall(characterId, "deathsaves", deathSaveData);
+    result.push(deathSaves);
+  };
+
+  return result;
+}
+
 export async function updateDDBCharacter(actor) {
   const characterId = actor.data.flags.ddbimporter.dndbeyond.characterId;
-
   const ddbData = await getCharacterData(characterId);
-
-  let promises = [];
 
   console.warn(actor.data);
   console.warn(ddbData);
 
-  // Fetch current character data and determine what needs to be updated
-
-  const currency = updateCharacterCall(characterId, "currency", actor.data.data.currency);
-  promises.push(currency);
-
-  const hitPointData = {
-    removedHitPoints: actor.data.data.attributes.hp.max - actor.data.data.attributes.hp.value,
-    temporaryHitPoints: actor.data.data.attributes.hp.temp,
-  };
-  const hitPoints = updateCharacterCall(characterId, "hitpoints", hitPointData);
-  promises.push(hitPoints);
+  let promises = [].concat(
+    currency(actor, characterId, ddbData),
+    hitPoints(actor, characterId, ddbData),
+    spellSlots(actor, characterId, ddbData),
+    inspiration(actor, characterId, ddbData),
+    exhaustion(actor, characterId, ddbData),
+    deathSaves(actor, characterId, ddbData),
+  );
 
   // for each action, check to see if it has uses, if yes:
   // const actionData = { actionId: "", entityTypeId: "", uses: "" };
   // const action = updateCharacterCall(characterId, "action/use", actionData);
   // promises.push(action);
 
-  const inspiration = updateCharacterCall(characterId, "inspiration", {
-    inspiration: actor.data.data.attributes.inspiration,
-  });
-  promises.push(inspiration);
-
-  let exhaustionData = {
-    conditionId: 4,
-    addCondition: false,
-  };
-  if (actor.data.data.attributes.exhaustion !== 0) {
-    exhaustionData["level"] = actor.data.data.attributes.exhaustion;
-    exhaustionData["totalHP"] = actor.data.data.attributes.hp.max;
-    exhaustionData["addCondition"] = true;
-  }
-
-  const exhaustion = updateCharacterCall(characterId, "condition", exhaustionData);
-  promises.push(exhaustion);
-
-  const deathSaveData = {
-    failCount: actor.data.data.attributes.death.failure,
-    successCount: actor.data.data.attributes.death.success,
-  };
-  const deathSaves = updateCharacterCall(characterId, "deathsaves", deathSaveData);
-  promises.push(deathSaves);
 
   // if a prepared spell caster
   // for each prepared spell that was added or removed
@@ -102,42 +185,6 @@ export async function updateDDBCharacter(actor) {
   // };
   // const spellsPrepared = updateCharacterCall(characterId, "spells/prepare", spellPreparedData);
   // promises.push(spellsPrepared);
-
-
-  //   spells:
-  // pact: {value: 0, override: null, max: 0, level: 0}
-  // spell0: {value: 0, max: 0}
-  // spell1: {value: 4, override: null, max: 4}
-  // spell2: {value: 3, override: null, max: 3}
-  // spell3: {value: 0, override: null, max: 0}
-  // spell4: {value: 0, override: null, max: 0}
-  // spell5: {value: 0, override: null, max: 0}
-  // spell6: {value: 0, override: null, max: 0}
-  // spell7: {value: 0, override: null, max: 0}
-  // spell8: {value: 0, override: null, max: 0}
-  // spell9: {value: 0, override: null, max: 0}
-
-  if (actor.data.data.spells.pact.max > 0) {
-    const used = actor.data.data.spells.pact.max - actor.data.data.spells.pact.value;
-    let spellSlotPackData = {
-      spellslots: {},
-      pact: true,
-    };
-    spellSlotPackData.spellslots[`level${actor.data.data.spells.pact.level}`] = used;
-    const spellPactSlots = updateCharacterCall(characterId, "spells/pact", spellSlotPackData);
-    promises.push(spellPactSlots);
-  }
-
-  let spellSlotData = { spellslots: {} };
-  for (let i = 1; i <= 9; i++) {
-    let spellData = actor.data.data.spells[`spell${i}`];
-    if (spellData.max > 0) {
-      const used = spellData.max - spellData.value;
-      spellSlotData.spellSlots[`spell${i}`] = used;
-    }
-  }
-  const spellSlots = updateCharacterCall(characterId, "spells/slots", spellSlotData);
-  promises.push(spellSlots);
 
   // if a known/choice spellcaster
   // and new spell/ spells removed
