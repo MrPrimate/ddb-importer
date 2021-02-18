b11b30f6c46839202cd73dcac1612613e2d59763
Jack Holloway <jack@mrprimate.co.uk>
2021-02-18 23:31:32 +0000

    effects: special additions list

---
+++ src/parser/effects/special.js
@@ -0,0 +1,31 @@
+// Bracers of Archery
+// +2 damage to longbows/shortbows translates to +2 ranged weapon damage
+
+// {
+//   "key": "data.bonuses.rwak.damage",
+//   "value": "+2",
+//   "mode": 0,
+//   "priority": 20
+// }
+
+// Demon Armor
+// Unarmed strikes bonus/weapons
+
+// {
+//   "key": "items.Unarmed Strike.data.attackBonus",
+//   "value": "1",
+//   "mode": 2,
+//   "priority": 20
+// },
+// {
+//   "key": "items.Unarmed Strike.data.damage.parts.0.0",
+//   "value": "1d8+@mod+1",
+//   "mode": 5,
+//   "priority": 20
+// },
+// {
+//   "key": "items.Unarmed Strike.data.properties.mgc",
+//   "value": "true",
+//   "mode": 5,
+//   "priority": 20
+// }
