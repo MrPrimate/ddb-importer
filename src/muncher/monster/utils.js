c1aa5d9de09f1144cf53bbb9fbc4c91ea9f5d392
Jack Holloway <jack@mrprimate.co.uk>
2021-03-04 20:54:53 +0000

    Monster: fix cone/line AoE

--- src/muncher/monster/utils.js
+++ src/muncher/monster/utils.js
@@ -370,8 +370,8 @@ export function getTarget(text) {
 
   // 90-foot line that is 10 feet wide
   // in a 90-foot cone
-  const coneSearch = /(\d+)-foot line/;
-  const lineSearch = /(\d+)-foot cone/;
+  const lineSearch = /(\d+)-foot line/;
+  const coneSearch = /(\d+)-foot cone/;
 
   const coneMatch = text.match(coneSearch);
   const lineMatch = text.match(lineSearch);
