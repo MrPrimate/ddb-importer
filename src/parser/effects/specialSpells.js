bac85e2abda074e8fb420116f1927e81cd2fe4ff
Jack Holloway <jack@mrprimate.co.uk>
2021-04-10 22:23:26 +0100

    DAE: absorb elements (future use)

--- src/parser/effects/specialSpells.js
+++ src/parser/effects/specialSpells.js
@@ -1,11 +1,52 @@
 // import { baseItemEffect } from "./effects.js";
 
-
 /**
  * This function is mainly for effects that can't be dynamically generated
  * @param {*} document
  */
 export function spellEffectAdjustment(document) {
-
+  switch (document.name) {
+    case "Absorb Elements": {
+      const effect = baseItemEffect(document, `${document.name} - Extra Damage`);
+      effect.changes.push({
+        key: "data.bonuses.mwak.damage",
+        value: `(@item.level)d6`,
+        mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+        priority: 0,
+      });
+      effect.transfer = false;
+      effect.disabled = false;
+      effect.flags.dae.transfer = false;
+      effect.flags.dae.stackable = false;
+      effect.flags.dae.specialDuration = "1Hit";
+      effect.duration = {
+        startTime: null,
+        seconds: null,
+        rounds: 1,
+        turns: null,
+        startRound: null,
+        startTurn: null,
+      };
+      document.data.damage = {
+        parts: [["", ""]],
+        versatile: "",
+        value: "",
+      };
+      document.data.target = {
+        value: null,
+        width: null,
+        units: "",
+        type: "self",
+      };
+      document.data.range = {
+        value: null,
+        long: null,
+        units: "self",
+      };
+      document.effects.push(effect);
+      break;
+    }
+    // no default
+  }
   return document;
 }
