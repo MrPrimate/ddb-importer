7f21d4a0c2793b4b13f3eec4cb9a2a4e9c250da6
Jack Holloway <jack@mrprimate.co.uk>
2021-05-27 21:13:57 +0100

    Ignore update message option

--- src/hooks/ready/checkVersion.js
+++ src/hooks/ready/checkVersion.js
@@ -30,7 +30,7 @@ async function getLatestModuleMinimumCoreVersion() {
 export default async () => {
   // check version number only for GMs
   const check = game.settings.get("ddb-importer", "update-check");
-  if (check && !game.user.isGM) return;
+  if (!check || !game.user.isGM) return;
 
   const moduleInfo = game.modules.get(MODULE_NAME).data;
   const installedVersion = moduleInfo.version;
