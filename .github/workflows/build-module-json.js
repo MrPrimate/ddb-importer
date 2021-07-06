05483dbad1458ca3f1466112d7e218064f4be2f8
Jack Holloway <jack@mrprimate.co.uk>
2021-07-06 19:28:50 +0100

    New CICD for v2.1.22

---
+++ .github/workflows/build-module-json.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+
+var mod = JSON.parse(fs.readFileSync('module.json', 'utf8'));
+
+mod.version = version;
+mod.download = `https://github.com/mrprimate/ddb-importer/releases/download/${version}/ddb-importer.zip`;
+
+console.log(JSON.stringify(mod));
--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,2 +1,2 @@
 var fs = require('fs');
-console.log(JSON.parse(fs.readFileSync('module.json', 'utf8')).version);
+console.log(JSON.parse(fs.readFileSync('package.json', 'utf8')).version);
