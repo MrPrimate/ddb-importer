00eb9f04ef4f5e4d93779a2fc521f0b96282de86
Jack Holloway <jack@mrprimate.co.uk>
2021-11-28 19:41:15 +0000

    Conditions outline

---
+++ src/character/conditions.js
@@ -0,0 +1,29 @@
+
+
+const CONDITION_MATRIX = [
+  { label: "Blinded", statusId: "Convenient Effect: Blinded", ddbId: 1, levelId: null, ddbType: 1 },
+  { label: "Charmed", statusId: "Convenient Effect: Charmed", ddbId: 2, level: null, ddbType: 1 },
+  { label: "Deafened", statusId: "Convenient Effect: Deafened", ddbId: 3, level: null, ddbType: 1 },
+  { label: "Exhaustion 1", statusId: "Convenient Effect: Exhaustion 1", ddbId: 4, levelId: 1, ddbType: 2 },
+  { label: "Exhaustion 2", statusId: "Convenient Effect: Exhaustion 2", ddbId: 4, levelId: 2, ddbType: 2 },
+  { label: "Exhaustion 3", statusId: "Convenient Effect: Exhaustion 3", ddbId: 4, levelId: 3, ddbType: 2 },
+  { label: "Exhaustion 4", statusId: "Convenient Effect: Exhaustion 4", ddbId: 4, levelId: 4, ddbType: 2 },
+  { label: "Exhaustion 5", statusId: "Convenient Effect: Exhaustion 5", ddbId: 4, levelId: 5, ddbType: 2 },
+  { label: "Exhaustion 6", statusId: "Convenient Effect: Exhaustion 6", ddbId: 4, levelId: 6, ddbType: 2 },
+  { label: "Frightened", statusId: "Convenient Effect: Frightened", ddbId: 5, level: null, ddbType: 1 },
+  { label: "Grappled", statusId: "Convenient Effect: Grappled", ddbId: 6, level: null, ddbType: 1 },
+  { label: "Incapacitated", statusId: "Convenient Effect: Incapacitated", ddbId: 7, level: null, ddbType: 1 },
+  { label: "Invisible", statusId: "Convenient Effect: Invisible", ddbId: 8, level: null, ddbType: 1 },
+  { label: "Paralyzed", statusId: "Convenient Effect: Paralyzed", ddbId: 9, level: null, ddbType: 1 },
+  { label: "Petrified", statusId: "Convenient Effect: Petrified", ddbId: 10, level: null, ddbType: 1 },
+  { label: "Poisoned", statusId: "Convenient Effect: Poisoned", ddbId: 11, level: null, ddbType: 1 },
+  { label: "Prone", statusId: "Convenient Effect: Prone", ddbId: 12, level: null, ddbType: 1 },
+  { label: "Restrained", statusId: "Convenient Effect: Restrained", ddbId: 13, level: null, ddbType: 1 },
+  { label: "Stunned", statusId: "Convenient Effect: Stunned", ddbId: 14, level: null, ddbType: 1 },
+  { label: "Unconscious", statusId: "Convenient Effect: Unconscious", ddbId: 15, level: null, ddbType: 1 },
+];
+
+
+export function setConditions(ddb, character) {
+
+}
