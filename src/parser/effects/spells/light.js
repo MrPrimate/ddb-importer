72ee77027a0a970b9c2eb0c4ecc73470ccefc751
Jack Holloway <jack@mrprimate.co.uk>
2022-01-01 16:25:53 +0000

    Light effect

---
+++ src/parser/effects/spells/light.js
@@ -0,0 +1,19 @@
+import { baseSpellEffect, generateATLChange } from "../specialSpells.js";
+import utils from "../../../utils.js";
+
+export function lightEffect(document) {
+  let effect = baseSpellEffect(document, document.name);
+
+  if (utils.isModuleInstalledAndActive("ATL")) {
+    effect.changes.push(generateATLChange("ATL.dimLight", CONST.ACTIVE_EFFECT_MODES.OVERRIDE, '40'));
+    effect.changes.push(generateATLChange("ATL.brightLight", CONST.ACTIVE_EFFECT_MODES.OVERRIDE, '20'));
+    effect.changes.push(generateATLChange("ATL.lightColor", CONST.ACTIVE_EFFECT_MODES.OVERRIDE, '#ffffff'));
+    effect.changes.push(generateATLChange("ATL.lightAlpha", CONST.ACTIVE_EFFECT_MODES.OVERRIDE, '0.25'));
+    const lightAnimation = '{"type": "pulse", "speed": 3,"intensity": 1}';
+    effect.changes.push(generateATLChange("ATL.lightAnimation", CONST.ACTIVE_EFFECT_MODES.OVERRIDE, lightAnimation));
+  }
+
+  document.effects.push(effect);
+
+  return document;
+}
