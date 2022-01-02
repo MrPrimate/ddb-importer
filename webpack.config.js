7bca4f4e1de46554233e43f1a48fc25db39475d8
Jack Holloway <jack@mrprimate.co.uk>
2022-01-02 19:31:11 +0000

    debug tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  devtool: "source-map",
   entry: './src/index.js',
   optimization: {
-    minimize: false
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
+
