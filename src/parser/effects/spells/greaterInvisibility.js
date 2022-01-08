8077bf01424ece57a4a0c4db2bf3c7976d872daa
Jack Holloway <jack@mrprimate.co.uk>
2022-01-08 14:26:26 +0000

    Spell Effects: invis

--- src/parser/effects/spells/greaterInvisibility.js
+++ src/parser/effects/spells/greaterInvisibility.js
@@ -4,20 +4,19 @@ export function greaterInvisibilityEffect(document) {
   let effect = baseSpellEffect(document, document.name);
   // MACRO START
   const itemMacroText = `
-//DAE Item Macro, no arguments passed
-if (!game.modules.get("advanced-macros")?.active) {ui.notifications.error("Please enable the Advanced Macros module") ;return;}
+if(!game.modules.get("advanced-macros")?.active) ui.notifications.error("Please enable the Advanced Macros module")
 
 const lastArg = args[args.length - 1];
-const target = canvas.tokens.get(lastArg.tokenId)
-
+const target = await fromUuid(lastArg.tokenUuid);
 
 if (args[0] === "on") {
-    ChatMessage.create({ content: \`\${target.name} turns invisible\`, whisper: [game.user] });
-    target.update({ "hidden": true });
+  await target.update({hidden : true}); // hide targeted token
+  ChatMessage.create({content: target.name + " turns invisible", whisper: [game.user] });
+
 }
-if (args[0] === "off") {
-    ChatMessage.create({ content: \`\${target.name} re-appears\`, whisper: [game.user] });
-    target.update({ "hidden": false });
+if (args[0]=== "off") {
+  await target.update({hidden : false}); // unhide token
+  ChatMessage.create({content: target.name + " re-appears", whisper: [game.user] });
 }
 `;
   // MACRO STOP
