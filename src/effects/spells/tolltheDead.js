8468f35965d4bf2441966aa2b0963f8105d80273
Jack Holloway <jack@mrprimate.co.uk>
2022-01-14 23:50:07 +0000

    Effects: Toll the Dead

--- src/effects/spells/tolltheDead.js
+++ src/effects/spells/tolltheDead.js
@@ -1,29 +1,10 @@
-const lastArg = args[args.length - 1];
+import { loadMacroFile, generateMacroFlags } from "../macros.js";
 
-if (lastArg.failedSaveUuids.length > 0) {
-  const tokenOrActor = await fromUuid(lastArg.actorUuid);
-  const casterActor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
-  const casterToken = await fromUuid(lastArg.tokenUuid);
-  const damageType = "necrotic";
-  const targets = await Promise.all(lastArg.failedSaveUuids.map(async (uuid) => await fromUuid(uuid)));
+export async function tolltheDeadEffect(document) {
+  const itemMacroText = await loadMacroFile("spell", "tolltheDeadEffect.js");
+  setProperty(document, "flags.itemacro", generateMacroFlags(document, itemMacroText));
+  setProperty(document, "flags.midi-qol.onUseMacroName", "[postActiveEffects]ItemMacro");
 
-  // assuming single target for spell
-  const damageDiceType = targets[0].actor.data.data.attributes.hp.max != targets[0].actor.data.data.attributes.hp.value ? 12 : 8;
-  const casterLevel = casterActor.data.type === "character" ? casterActor.data.data.details.level : casterActor.data.data.details.spellLevel;
-  const damageDiceNum = Math.floor((casterLevel + 1) / 6) + 1;
-  const damageRoll = await new Roll(`${damageDiceNum}d${damageDiceType}[${damageType}]`).evaluate({ async: true });
-  await new MidiQOL.DamageOnlyWorkflow(
-    casterActor,
-    casterToken,
-    damageRoll.total,
-    damageType,
-    targets,
-    damageRoll,
-    {
-      flavor: `(${CONFIG.DND5E.damageTypes[damageType]})`,
-      itemCardId: lastArg.itemCardId,
-      itemData: lastArg.item,
-      isCritical: lastArg.isCritical,
-    }
-  );
+  document.data.damage = { parts: [], versatile: "", value: "" };
+  return document;
 }
