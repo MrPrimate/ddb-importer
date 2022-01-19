a2f9fa3fff1ad008c6f6bbcf69528d564a647359
Jack Holloway <jack@mrprimate.co.uk>
2022-01-19 21:52:46 +0000

    Blindness Effect: uses ATL

--- macros/spells/blindnessDeafness.js
+++ macros/spells/blindnessDeafness.js
@@ -1,5 +1,11 @@
-if(!game.modules.get("advanced-macros")?.active) {ui.notifications.error("Please enable the Advanced Macros module") ;return;}
-if(!game.modules.get("dfreds-convenient-effects")?.active) {ui.notifications.error("Please enable the CE module"); return;}
+if (!game.modules.get("advanced-macros")?.active) {
+  ui.notifications.error("Please enable the Advanced Macros module");
+  return;
+}
+if (!game.modules.get("dfreds-convenient-effects")?.active) {
+  ui.notifications.error("Please enable the CE module");
+  return;
+}
 
 const lastArg = args[args.length - 1];
 const tokenOrActor = await fromUuid(lastArg.actorUuid);
@@ -15,33 +21,51 @@ function effectAppliedAndActive(conditionName) {
 }
 
 if (args[0] === "on") {
-    new Dialog({
-        title: "Choose an Effect",
-        buttons: {
-            one: {
-                label: "Blindness",
-                callback: () => {
-                    DAE.setFlag(targetActor, "DAEBlind", "blind")
-                    game.dfreds.effectInterface.addEffect("Blinded", targetActor.uuid)
-                }
+  new Dialog({
+    title: "Choose an Effect",
+    buttons: {
+      blind: {
+        label: "Blindness",
+        callback: () => {
+          DAE.setFlag(targetActor, "DAEBlind", "blind");
+          game.dfreds.effectInterface.addEffect({ effectName: "Blinded", uuid: targetActor.uuid });
+          const changes = [
+            {
+              key: "ATL.brightSight",
+              mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+              priority: 99,
+              value: "0",
             },
-            two: {
-                label: "Deafness",
-                callback: () => {
-                    DAE.setFlag(targetActor, "DAEBlind", "deaf")
-                    game.dfreds.effectInterface.addEffect("Deafened", targetActor.uuid)
-                }
-            }
+            {
+              key: "ATL.dimSight",
+              mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+              priority: 99,
+              value: "0",
+            },
+          ];
+          const effect = targetActor.effects.find((e) => e.data.label === lastArg.efData.label);
+          effect.update({ changes: changes.concat(effect.data.changes) });
         },
-    }).render(true);
+      },
+      deaf: {
+        label: "Deafness",
+        callback: () => {
+          DAE.setFlag(targetActor, "DAEBlind", "deaf");
+          game.dfreds.effectInterface.addEffect({ effectName: "Deafened", uuid: targetActor.uuid });
+        },
+      },
+    },
+  }).render(true);
 }
 
 if (args[0] === "off") {
-    let flag = DAE.getFlag(targetActor, "DAEBlind")
-    if (flag === "blind") {
-        if (effectAppliedAndActive("Blinded", targetActor)) game.dfreds.effectInterface.removeEffect("Blinded", targetActor.uuid)
-    } else if (flag === "deaf") {
-        if (effectAppliedAndActive("Deafened", targetActor)) game.dfreds.effectInterface.removeEffect("Deafened", targetActor.uuid)
-    }
-    DAE.unsetFlag(targetActor, "DAEBlind")
+  let flag = DAE.getFlag(targetActor, "DAEBlind");
+  if (flag === "blind") {
+    if (effectAppliedAndActive("Blinded", targetActor))
+      game.dfreds.effectInterface.removeEffect({ effectName: "Blinded", uuid: targetActor.uuid });
+  } else if (flag === "deaf") {
+    if (effectAppliedAndActive("Deafened", targetActor))
+      game.dfreds.effectInterface.removeEffect({ effectName: "Deafened", uuid: targetActor.uuid });
+  }
+  DAE.unsetFlag(targetActor, "DAEBlind");
 }
