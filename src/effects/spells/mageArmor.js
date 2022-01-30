cd929712069128e97b494bad8e9f8f4d09651156
Jack Holloway <jack@mrprimate.co.uk>
2022-01-30 09:32:42 +0000

    Mage armor notes

--- src/effects/spells/mageArmor.js
+++ src/effects/spells/mageArmor.js
@@ -2,12 +2,19 @@ import { baseSpellEffect } from "../specialSpells.js";
 
 export function mageArmorEffect(document) {
   let effect = baseSpellEffect(document, document.name);
+  // we use base here as it stacks better with other ac effects rather than the default mage calc
   effect.changes.push({
     key: "data.attributes.ac.base",
     mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
     value: "13",
     priority: "5",
   });
+  // effect.changes.push({
+  //   key: "data.attributes.ac.calc",
+  //   mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+  //   value: "mage",
+  //   priority: "5",
+  // });
   document.effects.push(effect);
 
   return document;
