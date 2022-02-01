d04d2c9036cfef59e322d337be752f8114103c47
Jack Holloway <jack@mrprimate.co.uk>
2022-02-01 09:15:50 +0000

    Mage Armor Effect: Fix

--- src/effects/spells/mageArmor.js
+++ src/effects/spells/mageArmor.js
@@ -2,19 +2,12 @@ import { baseSpellEffect } from "../specialSpells.js";
 
 export function mageArmorEffect(document) {
   let effect = baseSpellEffect(document, document.name);
-  // we use base here as it stacks better with other ac effects rather than the default mage calc
   effect.changes.push({
-    key: "data.attributes.ac.base",
+    key: "data.attributes.ac.calc",
     mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
-    value: "13",
+    value: "mage",
     priority: "5",
   });
-  // effect.changes.push({
-  //   key: "data.attributes.ac.calc",
-  //   mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
-  //   value: "mage",
-  //   priority: "5",
-  // });
   document.effects.push(effect);
 
   return document;
