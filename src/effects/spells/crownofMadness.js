1525f57068a2df7d75774c599ee2fb9ee963b7f1
Jack Holloway <jack@mrprimate.co.uk>
2022-02-22 09:34:28 +0000

    Crown of Madness effect

---
+++ src/effects/spells/crownofMadness.js
@@ -0,0 +1,15 @@
+import { baseSpellEffect, generateStatusEffectChange } from "../specialSpells.js";
+
+export function crownofMadnessEffect(document) {
+  let effect = baseSpellEffect(document, document.name);
+  effect.changes.push(generateStatusEffectChange("Charmed"));
+  effect.changes.push({
+    key: "flags.midi-qol.OverTime",
+    mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+    value: "label=Crown of Madness,turn=end,saveDc=@attributes.spelldc,saveAbility=wis,saveMagic=true",
+    priority: "20",
+  });
+  document.effects.push(effect);
+
+  return document;
+}
