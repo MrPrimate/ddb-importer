cb0509f497d6c42b73e8adf2b9584ad9256a10c5
Jack Holloway <jack@mrprimate.co.uk>
2022-02-26 18:51:18 +0000

    Horrifying Visage Updates

--- src/effects/monsterFeatures/horrifyingVisage.js
+++ src/effects/monsterFeatures/horrifyingVisage.js
@@ -1,11 +1,11 @@
-import { baseItemEffect } from "../effects.js";
+import { baseFeatEffect } from "../specialFeats.js";
 
 export function horrifyingVisageEffect(document) {
   setProperty(document, "data.duration", { value: 1, units: "minute" });
   setProperty(document, "data.target", { value: 60, width: null, units: "ft", type: "enemy" });
   setProperty(document, "data.range", { value: 60, long: null, units: "spec" });
 
-  let effect = baseItemEffect(document, document.name);
+  let effect = baseFeatEffect(document, document.name);
   effect.changes.push(
     {
       "key": "flags.midi-qol.OverTime",
