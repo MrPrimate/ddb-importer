d827db9c7bc0e8ab2761940aa9bb0020d7c7480c
Jack Holloway <jack@mrprimate.co.uk>
2022-02-26 20:47:40 +0000

    Rally Macro

---
+++ macros/feats/maneuversRally.js
@@ -0,0 +1,15 @@
+// pass in @spellLevel
+const lastArg = args[args.length - 1];
+const tokenOrActor = await fromUuid(lastArg.actorUuid);
+const target = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
+
+if (args[0] === "on") {
+  const dice = (parseInt(args[1]));
+  const curTemp = target.data.data.attributes.hp.temp;
+  const damageRoll = await new Roll(`${dice}[temphp]`).evaluate({ async: true });
+  if (game.dice3d) game.dice3d.showForRoll(damageRoll, game.users.get(options.userId));
+  const buf = damageRoll.total;
+  if (buf > curTemp) target.update({"data.attributes.hp.temp": buf});
+} else {
+  target.update({"data.attributes.hp.temp": 0});
+}
