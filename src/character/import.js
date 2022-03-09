3935ce1ebb69e04e01809a708576008d2b15b4b8
Jack Holloway <jack@mrprimate.co.uk>
2022-03-09 20:54:24 +0000

    remove debug

--- src/character/import.js
+++ src/character/import.js
@@ -1177,8 +1177,7 @@ export default class CharacterImport extends FormApplication {
     this.result.character.flags.ddbimporter["activeUpdate"] = false;
     this.result.character.flags.ddbimporter["activeSyncSpells"] = true;
     // remove unneeded flags (used for character parsing)
-    console.warn(this.result.character.flags.ddbimporter.dndbeyond["templateStrings"]);
-   // this.result.character.flags.ddbimporter.dndbeyond["templateStrings"] = null;
+    this.result.character.flags.ddbimporter.dndbeyond["templateStrings"] = null;
     this.result.character.flags.ddbimporter.dndbeyond["characterValues"] = null;
     this.result.character.flags.ddbimporter.dndbeyond["proficiencies"] = null;
     this.result.character.flags.ddbimporter.dndbeyond["proficienciesIncludingEffects"] = null;
