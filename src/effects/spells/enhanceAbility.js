cebf7e2e0d8789665420909e14e5abcbb48d933f
Jack Holloway <jack@mrprimate.co.uk>
2022-03-19 09:35:56 +0000

    Effects: enhance ability should run as local macro

--- src/effects/spells/enhanceAbility.js
+++ src/effects/spells/enhanceAbility.js
@@ -5,7 +5,7 @@ export async function enhanceAbilityEffect(document) {
   let effect = baseSpellEffect(document, document.name);
   const itemMacroText = await loadMacroFile("spell", "enhanceAbility.js");
   document.flags["itemacro"] = generateItemMacroFlag(document, itemMacroText);
-  effect.changes.push(generateMacroChange(""));
+  effect.changes.push(generateMacroChange("", 20, true));
   document.effects.push(effect);
 
   return document;
