976423fdd60658aa3fa25bdeb7263dedb66549ce
Jack Holloway <jack@mrprimate.co.uk>
2022-03-28 20:32:39 +0100

    bb self

--- src/effects/spells/boomingBlade.js
+++ src/effects/spells/boomingBlade.js
@@ -4,6 +4,8 @@ export async function boomingBladeEffect(document) {
   const itemMacroText = await loadMacroFile("spell", "boomingBlade.js");
   setProperty(document, "flags.itemacro", generateItemMacroFlag(document, itemMacroText));
   document.data.damage = { parts: [], versatile: "", value: "" };
+  document.data['target']['type'] = "self";
+  document.data.range = { value: null, units: "self", long: null };
   setProperty(document, "flags.midi-qol.onUseMacroName", "[postActiveEffects]ItemMacro");
   return document;
 }
