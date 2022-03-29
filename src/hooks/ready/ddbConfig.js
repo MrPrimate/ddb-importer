c2904a59659f0cc0913b9e4a1648d8f582fc59fa
Jack Holloway <jack@mrprimate.co.uk>
2022-03-29 22:49:33 +0100

    ddb fallback

--- src/hooks/ready/ddbConfig.js
+++ src/hooks/ready/ddbConfig.js
@@ -1,11 +1,23 @@
 import logger from "../../logger.js";
+import { fallbackDDBConfig } from "./fallbackConfig.js";
 
 export function loadDDBConfig() {
   if (!CONFIG.DDB) {
-    $.getJSON("https://www.dndbeyond.com/api/config/json").then((config) => {
-      setProperty(CONFIG, "DDB", config);
-      logger.debug("DDB_CONFIG", config);
-    });
+    try {
+      $.getJSON("https://www.dndbeyond.com/api/config/json").then((config) => {
+        if (config && config.sources) {
+          setProperty(CONFIG, "DDB", config);
+          logger.debug("Loaded DDB live config");
+        } else {
+          setProperty(CONFIG, "DDB", fallbackDDBConfig);
+          logger.warn("Failed to load DDB config, using fallback.");
+        }
+        logger.debug("DDB_CONFIG", CONFIG.DDB);
+      });
+    } catch (err) {
+      setProperty(CONFIG, "DDB", fallbackDDBConfig);
+      logger.warn("Failed to load DDB config, using fallback.");
+    }
   }
 }
 
