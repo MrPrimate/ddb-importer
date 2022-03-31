555db86a093e0a6ad06ef2b66958889bcd431883
Jack Holloway <jack@mrprimate.co.uk>
2022-03-31 08:59:21 +0100

    More config checls

--- src/hooks/ready/ddbConfig.js
+++ src/hooks/ready/ddbConfig.js
@@ -5,21 +5,26 @@ export function loadDDBConfig() {
   if (!CONFIG.DDB) {
     setProperty(CONFIG, "DDB", fallbackDDBConfig);
     logger.info("Loaded default DDB config, checking for live config access.");
-    try {
-      $.getJSON("https://www.dndbeyond.com/api/config/json").then((config) => {
+    $.getJSON("https://www.dndbeyond.com/api/config/json")
+      .then((config) => {
         if (config && config.sources) {
           setProperty(CONFIG, "DDB", config);
           logger.info("Loaded DDB live config");
-        } else {
+        }
+        return config;
+      })
+      .catch((err) => {
+        logger.warn("Failed to load DDB config, caught error using fallback.", err);
+        return err;
+      })
+      .always(() => {
+        if (!CONFIG.DDB?.sources) {
           setProperty(CONFIG, "DDB", fallbackDDBConfig);
           logger.warn("Failed to load DDB config, using fallback.");
+        } else {
+          logger.info("A DDB config was loaded");
         }
+        logger.debug("DDB_CONFIG", CONFIG.DDB);
       });
-    } catch (err) {
-      setProperty(CONFIG, "DDB", fallbackDDBConfig);
-      logger.warn("Failed to load DDB config, using fallback.");
-    }
-    logger.debug("DDB_CONFIG", CONFIG.DDB);
   }
 }
-
