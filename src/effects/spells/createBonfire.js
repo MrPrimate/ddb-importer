0b26c84d136933d2266a3b0e9601e71df7909990
Jack Holloway <jack@mrprimate.co.uk>
2022-04-09 15:04:08 +0100

    Effect: Create Bonfire outline

---
+++ src/effects/spells/createBonfire.js
@@ -0,0 +1,50 @@
+import { baseSpellEffect, spellEffectModules } from "../specialSpells.js";
+import { loadMacroFile, generateMacroChange, generateItemMacroFlag } from "../macros.js";
+
+export async function createBonfireEffect(document) {
+  // we require active auras for this effect
+  if (!spellEffectModules().activeAurasInstalled) return document;
+
+  const itemMacroText = await loadMacroFile("generic", "activeAuraDamageOnEntry.js");
+  document.flags["itemacro"] = generateItemMacroFlag(document, itemMacroText);
+
+  let effect = baseSpellEffect(document, document.name);
+  effect.changes.push(
+    {
+      key: "flags.midi-qol.OverTime",
+      mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+      value:
+        "turn=end,label=Create Bonfire,damageRoll=(@level)d8,damageType=fire,saveRemove=false,saveDC=@attributes.spelldc,saveAbility=dex,saveDamage=nodamage,killAnim=true",
+      priority: "20",
+    }
+  );
+  effect.flags["ActiveAuras"] = {
+    isAura: true,
+    aura: "All",
+    radius: null,
+    alignment: "",
+    type: "",
+    ignoreSelf: false,
+    height: false,
+    hidden: false,
+    // hostile: true,
+    onlyOnce: false,
+    save: "dex",
+    savedc: null,
+    displayTemp: true,
+  };
+  setProperty(effect, "duration.seconds", 60);
+  effect.changes.push(generateMacroChange(""));
+  setProperty(document, "flags.midi-qol.onUseMacroName", "[preActiveEffects]ItemMacro");
+
+  document.effects.push(effect);
+
+  setProperty(document, "flags.ddbimporter.effect", {
+    dice: document.data.damage.parts[0][0],
+    damageType: document.data.damage.parts[0][1],
+    save: document.data.save.ability,
+    sequencerFile: "jb2a.flames.01.orange",
+  });
+
+  return document;
+}
