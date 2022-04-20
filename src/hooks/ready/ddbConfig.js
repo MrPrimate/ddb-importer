57164b45d6c51dbb5c3954a7eece3334ae6f57bd
Jack Holloway <jack@mrprimate.co.uk>
2022-04-20 08:58:02 +0100

    DDB Config now falls back to proxy

--- src/hooks/ready/ddbConfig.js
+++ src/hooks/ready/ddbConfig.js
@@ -1,35 +1,79 @@
 import logger from "../../logger.js";
 import { fallbackDDBConfig } from "./fallbackConfig.js";
+import { getCobalt } from "../../lib/Secrets.js";
+import { download } from "../../muncher/utils.js";
+
+function directConfig() {
+  $.getJSON("https://www.dndbeyond.com/api/config/json")
+    .then((config) => {
+      if (config && config.sources) {
+        setProperty(CONFIG, "DDB", config);
+        logger.info("Loaded DDB live config");
+      }
+      return config;
+    })
+    .catch((err) => {
+      logger.warn("Failed to load DDB config, caught error using fallback.", err);
+      return err;
+    })
+    .always(() => {
+      if (!CONFIG.DDB?.sources) {
+        setProperty(CONFIG, "DDB", fallbackDDBConfig);
+        logger.warn("Failed to load DDB config, using fallback.");
+      } else {
+        logger.info("A DDB config was loaded");
+      }
+      logger.debug("DDB_CONFIG", CONFIG.DDB);
+    });
+}
+
+function proxyConfig() {
+  const parsingApi = game.settings.get("ddb-importer", "api-endpoint");
+  const debugJson = game.settings.get("ddb-importer", "debug-json");
+
+  const url = `${parsingApi}/proxy/api/config/json`;
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.success) {
+          logger.error(`API Failure: ${data.message}`);
+          reject(data.message);
+        }
+        if (debugJson) {
+          download(JSON.stringify(data), `monsters-raw.json`, "application/json");
+        }
+        return data;
+      })
+      .then((data) => {
+        logger.info(`Retrieved DDB CONFIG DATA via proxy`);
+        setProperty(CONFIG, "DDB", data.data);
+        logger.debug("DDB_CONFIG", CONFIG.DDB);
+        resolve(data.data);
+      })
+      .catch((error) => reject(error));
+  });
+}
 
 export function loadDDBConfig() {
-  if (!CONFIG.DDB) {
+  if (!hasProperty(CONFIG, "DDB")) {
     setProperty(CONFIG, "DDB", fallbackDDBConfig);
-    if ((/electron/i).test(navigator.userAgent)) {
-      logger.info("Electron detected using DDB Config stub");
-      logger.debug("DDB_CONFIG", CONFIG.DDB);
+    if (getProperty(CONFIG, "DEBUG.DDBI.DIRECT_CONFIG")) {
+      if ((/electron/i).test(navigator.userAgent)) {
+        logger.info("Electron detected using DDB Config stub");
+        logger.debug("DDB_CONFIG", CONFIG.DDB);
+      } else {
+        logger.info("Loaded default DDB config, checking for live config access.");
+        directConfig();
+      }
     } else {
-      logger.info("Loaded default DDB config, checking for live config access.");
-      $.getJSON("https://www.dndbeyond.com/api/config/json")
-        .then((config) => {
-          if (config && config.sources) {
-            setProperty(CONFIG, "DDB", config);
-            logger.info("Loaded DDB live config");
-          }
-          return config;
-        })
-        .catch((err) => {
-          logger.warn("Failed to load DDB config, caught error using fallback.", err);
-          return err;
-        })
-        .always(() => {
-          if (!CONFIG.DDB?.sources) {
-            setProperty(CONFIG, "DDB", fallbackDDBConfig);
-            logger.warn("Failed to load DDB config, using fallback.");
-          } else {
-            logger.info("A DDB config was loaded");
-          }
-          logger.debug("DDB_CONFIG", CONFIG.DDB);
-        });
+      proxyConfig();
     }
   }
 }
