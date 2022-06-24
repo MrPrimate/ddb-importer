204a0911b2854c2ed3f2f847c706845dac86162c
Jack Holloway <jack@mrprimate.co.uk>
2022-06-24 18:46:27 +0100

    Item Collection adjustments for v10 alpha

--- src/character/itemCollections.js
+++ src/character/itemCollections.js
@@ -1,18 +1,18 @@
 export function getItemCollectionItems(actor) {
-  const characterId = getProperty(actor.data, "flags.ddbimporter.dndbeyond.characterId");
+  const characterId = getProperty(actor, "flags.ddbimporter.dndbeyond.characterId");
   if (!game.modules.get("itemcollection")?.active && !Number.isInteger(characterId)) return [];
   const topLevelItems = actor.items
     .filter((item) =>
-      hasProperty(item.data, "flags.ddbimporter.id") &&
-      hasProperty(item.data, "flags.ddbimporter.containerEntityId") &&
-      item.data.flags.ddbimporter.containerEntityId === parseInt(characterId) &&
-      !item.data.flags.ddbimporter?.ignoreItemImport
+      hasProperty(item, "flags.ddbimporter.id") &&
+      hasProperty(item, "flags.ddbimporter.containerEntityId") &&
+      item.flags.ddbimporter.containerEntityId === parseInt(characterId) &&
+      !item.flags.ddbimporter?.ignoreItemImport
     );
 
   const itemCollectionItems = topLevelItems
     .map((topLevelItem) => {
-      const containerId = getProperty(topLevelItem.data, "flags.ddbimporter.id");
-      const items = (getProperty(topLevelItem.data.flags, "itemcollection.contentsData") ?? [])
+      const containerId = getProperty(topLevelItem, "flags.ddbimporter.id");
+      const items = (getProperty(topLevelItem.flags, "itemcollection.contentsData") ?? [])
         .map((item) => {
           setProperty(item, "flags.ddbimporter.containerEntityId", containerId);
           setProperty(item, "flags.ddbimporter.updateDocumentId", topLevelItem.id);
@@ -30,17 +30,17 @@ export async function addContainerItemsToContainers(ddb, actor) {
 
   const topLevelItems = actor.items
     .filter((item) =>
-      hasProperty(item.data, "flags.ddbimporter.id") &&
-      hasProperty(item.data, "flags.ddbimporter.containerEntityId") &&
-      item.data.flags.ddbimporter.containerEntityId === ddb.character.id &&
-      !item.data.flags.ddbimporter?.ignoreItemImport
+      hasProperty(item, "flags.ddbimporter.id") &&
+      hasProperty(item, "flags.ddbimporter.containerEntityId") &&
+      item.flags.ddbimporter.containerEntityId === ddb.character.id &&
+      !item.flags.ddbimporter?.ignoreItemImport
     );
 
   for (const topLevelItem of topLevelItems) {
     const itemsToImport = actor.items
       .filter((item) =>
-        hasProperty(item.data, "flags.ddbimporter.containerEntityId") &&
-        item.data.flags.ddbimporter.containerEntityId === topLevelItem.data.flags.ddbimporter.id
+        hasProperty(item, "flags.ddbimporter.containerEntityId") &&
+        item.flags.ddbimporter.containerEntityId === topLevelItem.flags.ddbimporter.id
       )
       .map((item) => {
         return duplicate(item);
@@ -58,16 +58,16 @@ export async function addContainerItemsToContainers(ddb, actor) {
 export async function addContainerItemsToActor(ddb, actor) {
   const topLevelItems = actor.items
     .filter((item) =>
-      hasProperty(item.data, "flags.ddbimporter.id") &&
-      hasProperty(item.data, "flags.ddbimporter.containerEntityId") &&
-      item.data.flags.ddbimporter.containerEntityId === ddb.character.id &&
-      hasProperty(item.data, "flags.itemcollection.contentsData") &&
-      item.data.flags.itemcollection.contentsData.length > 0 &&
-      !item.data.flags.ddbimporter?.ignoreItemImport
+      hasProperty(item, "flags.ddbimporter.id") &&
+      hasProperty(item, "flags.ddbimporter.containerEntityId") &&
+      item.flags.ddbimporter.containerEntityId === ddb.character.id &&
+      hasProperty(item, "flags.itemcollection.contentsData") &&
+      item.flags.itemcollection.contentsData.length > 0 &&
+      !item.flags.ddbimporter?.ignoreItemImport
     );
 
   for (const topLevelItem of topLevelItems) {
-    const itemsToImport = duplicate(getProperty(topLevelItem.data.flags, "itemcollection.contentsData") ?? [])
+    const itemsToImport = duplicate(getProperty(topLevelItem.flags, "itemcollection.contentsData") ?? [])
       .map((item) => {
         if (hasProperty(item, "flags.ddbimporter.updateDocumentId")) {
           delete item.flags.ddbimporter.updateDocumentId;
@@ -75,7 +75,7 @@ export async function addContainerItemsToActor(ddb, actor) {
         return item;
       });
     const itemsToDelete = itemsToImport.map((item) => item._id);
-    // const currency = duplicate(topLevelItem.data.data.currency);
+    // const currency = duplicate(topLevelItem.system.currency);
     await topLevelItem.deleteEmbeddedDocuments("Item", itemsToDelete);
     await actor.createEmbeddedDocuments("Item", itemsToImport, { keepId: true });
   }
