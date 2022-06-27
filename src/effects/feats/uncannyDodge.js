3f4fe3cb2b0bae3c4a4d3d99874c7f65621ae179
Jack Holloway <jack@mrprimate.co.uk>
2022-06-27 18:34:26 +0100

    Effect: Fix Uncanny Dodge

--- src/effects/feats/uncannyDodge.js
+++ src/effects/feats/uncannyDodge.js
@@ -1,12 +1,18 @@
-import { baseItemEffect } from "../effects.js";
+import { baseFeatEffect } from "../specialFeats.js";
 
 export function uncannyDodgeEffect(document) {
-  let effect = baseItemEffect(document, document.name);
-  effect.changes.push(
-    { key: "flags.midi-qol.uncanny-dodge", value: "1", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, priority: 10 },
-  );
-  effect.flags.dae.specialDuration = ["turnStart", "isDamaged"];
+  let effect = baseFeatEffect(document, "1/2 Damage");
+  effect.changes.push({
+    key: "flags.midi-qol.uncanny-dodge",
+    value: "1",
+    mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+    priority: 10,
+  });
+  effect.flags.dae.specialDuration = ["1Reaction"];
+  document.data["target"]["type"] = "self";
+  document.data.range = { value: null, units: "self", long: null };
+  document.data.actionType = null;
+  document.data.activation.type = "reactiondamage";
   document.effects.push(effect);
   return document;
 }
-
