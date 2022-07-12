4f4d7231b7ad7e4acc103b7ca5add1493999ea5b
Jack Holloway <jack@mrprimate.co.uk>
2022-07-12 19:00:57 +0100

    Entangle comments

--- src/effects/spells/entangle.js
+++ src/effects/spells/entangle.js
@@ -3,6 +3,17 @@ import { baseSpellEffect, generateStatusEffectChange } from "../specialSpells.js
 export function entangleEffect(document) {
   let effect = baseSpellEffect(document, document.name);
   effect.changes.push(generateStatusEffectChange("Restrained"));
+
+  // not implemented as the target can choose to escape with it's action
+  // effect.changes.push(
+  //   {
+  //     key: "flags.midi-qol.OverTime",
+  //     mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+  //     value:
+  //       `label=${document.name} (Start of Turn),turn=start, saveAbility=${document.data.save.ability}, saveDC=@attributes.spelldc, rollType=save, saveMagic=true, saveRemove=true`,
+  //     priority: "20",
+  //   },
+  // );
   document.effects.push(effect);
 
   return document;
