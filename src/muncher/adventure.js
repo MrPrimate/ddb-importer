7462c1d3b2f239d6ba74cfe98f9e381851b2faec
Jack Holloway <jack@mrprimate.co.uk>
2022-07-19 22:02:45 +0100

    Initial Vehicle outline

--- src/muncher/adventure.js
+++ src/muncher/adventure.js
@@ -1,38 +1,6 @@
-import { munchNote, getCampaignId, download, getCompendium, getCompendiumLabel } from "./utils.js";
+import { getCampaignId, download, getCompendium, getCompendiumLabel } from "./utils.js";
 import { getCobalt } from "../lib/Secrets.js";
-
-function getVehicleData() {
-  const cobaltCookie = getCobalt();
-  const campaignId = getCampaignId();
-  const parsingApi = game.settings.get("ddb-importer", "api-endpoint");
-  const betaKey = game.settings.get("ddb-importer", "beta-key");
-  const body = { cobalt: cobaltCookie, campaignId: campaignId, betaKey: betaKey };
-  const debugJson = game.settings.get("ddb-importer", "debug-json");
-
-  return new Promise((resolve, reject) => {
-    fetch(`${parsingApi}/proxy/vehicles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (debugJson) {
-          download(JSON.stringify(data), `vehicles-raw.json`, "application/json");
-        }
-        if (!data.success) {
-          munchNote(`Failure: ${data.message}`);
-          reject(data.message);
-        }
-        return data;
-      })
-      .then((data) => resolve(data.data))
-      .catch((error) => reject(error));
-  });
-}
-
+import { getVehicleData } from "./vehicle.js";
 
 async function getMonsterMap () {
   // ddb://monsters
---
+++ src/muncher/vehicle.js
@@ -0,0 +1,37 @@
+import { munchNote, getCampaignId, download, getCompendium, getCompendiumLabel } from "./utils.js";
+import { getCobalt } from "../lib/Secrets.js";
+
+export function getVehicleData() {
+  const cobaltCookie = getCobalt();
+  const campaignId = getCampaignId();
+  const parsingApi = game.settings.get("ddb-importer", "api-endpoint");
+  const betaKey = game.settings.get("ddb-importer", "beta-key");
+  const body = { cobalt: cobaltCookie, campaignId: campaignId, betaKey: betaKey };
+  const debugJson = game.settings.get("ddb-importer", "debug-json");
+
+  return new Promise((resolve, reject) => {
+    fetch(`${parsingApi}/proxy/vehicles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (debugJson) {
+          download(JSON.stringify(data), `vehicles-raw.json`, "application/json");
+        }
+        if (!data.success) {
+          munchNote(`Failure: ${data.message}`);
+          reject(data.message);
+        }
+        return data;
+      })
+      .then((data) => resolve(data.data))
+      .catch((error) => reject(error));
+  });
+}
+
+
+window.DDBTest = getVehicleData;
