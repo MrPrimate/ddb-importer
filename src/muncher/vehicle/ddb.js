f097fa4d35f30001d31e461d92dd9c91bfa1a1bf
Jack Holloway <jack@mrprimate.co.uk>
2022-07-25 20:11:16 +0100

    Add parsed vehicle loader

---
+++ src/muncher/vehicle/ddb.js
@@ -0,0 +1,155 @@
+export const ddbVehicleDictionary = {
+  objectTypes: [
+    {
+      type: "vehicle",
+      name: "Vehicle",
+    },
+  ],
+  vehicleComponentTypeAdjustmentTypes: [
+    {
+      type: "speed",
+      name: "Speed",
+    },
+  ],
+  vehicleMovementTypes: [
+    {
+      type: "magical",
+      name: "Magical",
+    },
+    {
+      type: "air",
+      name: "Air",
+    },
+    {
+      type: "land",
+      name: "Land",
+    },
+    {
+      type: "water",
+      name: "Water",
+    },
+  ],
+  componentTypes: [
+    {
+      type: "weapon",
+      name: "Weapon",
+    },
+    {
+      type: "control",
+      name: "Control",
+    },
+    {
+      type: "movement",
+      name: "Movement",
+    },
+    {
+      type: "helm",
+      name: "Helm",
+    },
+    {
+      type: "hull",
+      name: "Hull",
+    },
+  ],
+  coverTypes: null,
+  vehicleWeightRanges: [
+    {
+      sizeId: 4,
+      minSize: 50,
+    },
+    {
+      sizeId: 3,
+      minSize: 10,
+    },
+    {
+      sizeId: 5,
+      minSize: 200,
+    },
+    {
+      sizeId: 2,
+      minSize: 0,
+    },
+    {
+      sizeId: 3,
+      minSize: 10,
+    },
+    {
+      sizeId: 7,
+      minSize: 10000,
+    },
+    {
+      sizeId: 5,
+      minSize: 200,
+    },
+    {
+      sizeId: 2,
+      minSize: 0,
+    },
+    {
+      sizeId: 6,
+      minSize: 1000,
+    },
+    {
+      sizeId: 4,
+      minSize: 50,
+    },
+    {
+      sizeId: 7,
+      minSize: 10000,
+    },
+    {
+      sizeId: 6,
+      minSize: 1000,
+    },
+  ],
+  vehicleDimensionRanges: [
+    {
+      sizeId: 6,
+      minSize: 15,
+    },
+    {
+      sizeId: 4,
+      minSize: 5,
+    },
+    {
+      sizeId: 7,
+      minSize: 20,
+    },
+    {
+      sizeId: 6,
+      minSize: 15,
+    },
+    {
+      sizeId: 2,
+      minSize: 0,
+    },
+    {
+      sizeId: 2,
+      minSize: 0,
+    },
+    {
+      sizeId: 7,
+      minSize: 20,
+    },
+    {
+      sizeId: 3,
+      minSize: 3,
+    },
+    {
+      sizeId: 5,
+      minSize: 10,
+    },
+    {
+      sizeId: 3,
+      minSize: 3,
+    },
+    {
+      sizeId: 4,
+      minSize: 5,
+    },
+    {
+      sizeId: 5,
+      minSize: 10,
+    },
+  ],
+};
