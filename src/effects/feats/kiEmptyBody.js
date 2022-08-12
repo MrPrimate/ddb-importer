e920dbf9196b4479094e26c99be51573db3bd906
Jack Holloway <jack@mrprimate.co.uk>
2022-08-12 19:49:32 +0100

    Invisibility Updates

--- src/effects/feats/kiEmptyBody.js
+++ src/effects/feats/kiEmptyBody.js
@@ -1,20 +1,18 @@
 import { baseFeatEffect } from "../specialFeats.js";
-import { generateMacroChange, generateItemMacroFlag, loadMacroFile } from "../macros.js";
+import { generateStatusEffectChange } from "../effects.js";
 
 export async function kiEmptyBodyEffect(document) {
   let effect = baseFeatEffect(document, document.name);
   effect.changes.push(
     { key: "system.traits.dr.all", value: "", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, priority: 0 },
-    { key: "system.traits.dv.value", value: "force", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, priority: 0 }
+    { key: "system.traits.dv.value", value: "force", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, priority: 0 },
+    generateStatusEffectChange("invisible"),
   );
 
   document.system["target"]["type"] = "self";
   document.system.range = { value: null, units: "self", long: null };
   document.system.duration = { value: 1, units: "min" };
   document.system.actionType = null;
-  const itemMacroText = await loadMacroFile("spell", "invisibility.js");
-  document.flags["itemacro"] = generateItemMacroFlag(document, itemMacroText);
-  effect.changes.push(generateMacroChange("", 0));
 
   document.effects.push(effect);
   return document;
