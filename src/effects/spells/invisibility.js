b7d19f2f5c16c3b3a24e700a04ab900e0ff0b938
Jack Holloway <jack@mrprimate.co.uk>
2022-10-19 09:23:52 +0100

    Invisbility: default to CE

--- src/effects/spells/invisibility.js
+++ src/effects/spells/invisibility.js
@@ -2,7 +2,7 @@ import { baseSpellEffect, generateStatusEffectChange } from "../specialSpells.js
 
 export async function invisibilityEffect(document) {
   let effect = baseSpellEffect(document, document.name);
-  effect.changes.push(generateStatusEffectChange("invisible"));
+  effect.changes.push(generateStatusEffectChange("Convenient Effect: Invisible"));
 
   document.effects.push(effect);
 
