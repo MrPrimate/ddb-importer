1e8a871b6d1dd1264453b2ab04de10d1e0b64e57
Jack Holloway <jack@mrprimate.co.uk>
2022-10-27 15:05:36 +0100

    Paladin Aura: dynamic sizing

--- src/effects/feats/paladinDefaultAura.js
+++ src/effects/feats/paladinDefaultAura.js
@@ -1,13 +1,16 @@
 export function paladinDefaultAuraEffect(document) {
   document.effects.forEach((effect) => {
     if (effect.label.includes("Constant Effects")) {
+      const distance = document.flags.ddbimporter?.dndbeyond?.levelScale?.fixedValue ?? 10;
       effect.flags.ActiveAuras = {
         aura: "Allies",
-        radius: 10,
+        radius: distance,
         isAura: true,
         inactive: false,
         hidden: false,
+        displayTemp: true,
       };
+      setProperty(effect, "flags.core.statusId", 1);
     }
   });
   return document;
