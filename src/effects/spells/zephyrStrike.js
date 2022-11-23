d2d0136093c4f03ceadae44a591e1db066be0508
Jack Holloway <jack@mrprimate.co.uk>
2022-11-23 20:44:23 +0000

    Zephry strike outline

---
+++ src/effects/spells/zephyrStrike.js
@@ -0,0 +1,15 @@
+import { baseSpellEffect } from "../specialSpells.js";
+
+export function zephyrStrikeEffect(document) {
+  let effect = baseSpellEffect(document, document.name);
+  effect.changes.push({
+    key: "system.attributes.ac.calc",
+    mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+    value: "mage",
+    priority: "5",
+  });
+  document.effects.push(effect);
+
+
+  return document;
+}
