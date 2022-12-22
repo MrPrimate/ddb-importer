5aac79ec1f74a1ec3d7c2bbc982200396b8c1675
Jack Holloway <jack@mrprimate.co.uk>
2022-12-22 20:57:42 +0000

    Parse Companion Spells

--- src/parser/companions/companions.js
+++ src/parser/companions/companions.js
@@ -8,10 +8,28 @@ DDBCharacter.prototype.getClassFeature = function getClassFeature(name) {
   return klass?.classFeatures?.find((f) => f.definition.name == name);
 };
 
+DDBCharacter.prototype._getCompanionSpell = async function _getCompanionSpell(name) {
+  const spell = this.data.spells.find((s) => s.name === name || s.flags.ddbimporter?.originalName === name);
+  if (!spell) return [];
+  const ddbCompanionFactory = new DDBCompanionFactory(this, spell.system.description.value, {});
+  await ddbCompanionFactory.parse();
+  return ddbCompanionFactory.companions;
+};
+
+// DDBCharacter.prototype._getCompanionFeature = async function _getCompanionFeature(featureName) {
+//   const feature = this.getClassFeature(featureName);
+//   if (!feature) return [];
+//   const ddbCompanionFactory = new DDBCompanionFactory(this, feature.definition.description, {});
+//   await ddbCompanionFactory.parse();
+//   return ddbCompanionFactory.companions;
+// };
+
 DDBCharacter.prototype._getCompanionFeature = async function _getCompanionFeature(featureName) {
-  const feature = this.getClassFeature(featureName);
-  if (!feature) return undefined;
-  const ddbCompanionFactory = new DDBCompanionFactory(this, feature.definition.description, {});
+  const feature = this.data.features.concat(this.data.actions).find((s) =>
+    s.name === featureName || s.flags.ddbimporter?.originalName === featureName
+  );
+  if (!feature) return [];
+  const ddbCompanionFactory = new DDBCompanionFactory(this, feature.system.description.value, {});
   await ddbCompanionFactory.parse();
   return ddbCompanionFactory.companions;
 };
@@ -20,10 +38,33 @@ DDBCharacter.prototype.generateCompanions = async function generateCompanions() 
   if (!game.settings.get(SETTINGS.MODULE_ID, "character-update-policy-create-companions")) return;
   const steelDefender = await this._getCompanionFeature("Steel Defender");
   const infusions = await this._getCompanionFeature("Artificer Infusions");
+  const wildFireSpirit = await this._getCompanionFeature("Summon Wildfire Spirit");
+  const primalCompanions = await this._getCompanionFeature("Primal Companion");
+  const aberration = await this._getCompanionSpell("Summon Aberration");
+  const beast = await this._getCompanionSpell("Summon Beast");
+  const celestial = await this._getCompanionSpell("Summon Celestial");
+  const construct = await this._getCompanionSpell("Summon Construct");
+  const elemental = await this._getCompanionSpell("Summon Elemental");
+  const fey = await this._getCompanionSpell("Summon Fey");
+  const fiend = await this._getCompanionSpell("Summon Fiend");
+  const shadowspawn = await this._getCompanionSpell("Summon Shadowspawn");
+  const undead = await this._getCompanionSpell("Summon Undead");
+
 
   const companions = [
-    steelDefender,
-    infusions
+    ...steelDefender,
+    ...infusions,
+    ...wildFireSpirit,
+    ...primalCompanions,
+    ...aberration,
+    ...beast,
+    ...celestial,
+    ...construct,
+    ...elemental,
+    ...fey,
+    ...fiend,
+    ...shadowspawn,
+    ...undead,
   ];
 
 
