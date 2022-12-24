3a00beffcf11a31460f465d39554fe8e9e8f7d21
Jack Holloway <jack@mrprimate.co.uk>
2022-12-24 15:52:27 +0000

    DRY up conditions

--- src/parser/special/conditions.js
+++ src/parser/special/conditions.js
@@ -1,35 +1,14 @@
+import DICTIONARY from "../../dictionary.js";
 import logger from "../../logger.js";
 import SETTINGS from "../../settings.js";
 
-const CONDITION_MATRIX = [
-  { label: "Blinded", statusId: "Convenient Effect: Blinded", ddbId: 1, levelId: null, ddbType: 1 },
-  { label: "Charmed", statusId: "Convenient Effect: Charmed", ddbId: 2, levelId: null, ddbType: 1 },
-  { label: "Deafened", statusId: "Convenient Effect: Deafened", ddbId: 3, levelId: null, ddbType: 1 },
-  { label: "Exhaustion 1", statusId: "Convenient Effect: Exhaustion 1", ddbId: 4, levelId: 1, ddbType: 2 },
-  { label: "Exhaustion 2", statusId: "Convenient Effect: Exhaustion 2", ddbId: 4, levelId: 2, ddbType: 2 },
-  { label: "Exhaustion 3", statusId: "Convenient Effect: Exhaustion 3", ddbId: 4, levelId: 3, ddbType: 2 },
-  { label: "Exhaustion 4", statusId: "Convenient Effect: Exhaustion 4", ddbId: 4, levelId: 4, ddbType: 2 },
-  { label: "Exhaustion 5", statusId: "Convenient Effect: Exhaustion 5", ddbId: 4, levelId: 5, ddbType: 2 },
-  { label: "Exhaustion 6", statusId: "Convenient Effect: Exhaustion 6", ddbId: 4, levelId: 6, ddbType: 2 },
-  { label: "Frightened", statusId: "Convenient Effect: Frightened", ddbId: 5, levelId: null, ddbType: 1 },
-  { label: "Grappled", statusId: "Convenient Effect: Grappled", ddbId: 6, levelId: null, ddbType: 1 },
-  { label: "Incapacitated", statusId: "Convenient Effect: Incapacitated", ddbId: 7, levelId: null, ddbType: 1 },
-  { label: "Invisible", statusId: "Convenient Effect: Invisible", ddbId: 8, levelId: null, ddbType: 1 },
-  { label: "Paralyzed", statusId: "Convenient Effect: Paralyzed", ddbId: 9, levelId: null, ddbType: 1 },
-  { label: "Petrified", statusId: "Convenient Effect: Petrified", ddbId: 10, levelId: null, ddbType: 1 },
-  { label: "Poisoned", statusId: "Convenient Effect: Poisoned", ddbId: 11, levelId: null, ddbType: 1 },
-  { label: "Prone", statusId: "Convenient Effect: Prone", ddbId: 12, levelId: null, ddbType: 1 },
-  { label: "Restrained", statusId: "Convenient Effect: Restrained", ddbId: 13, levelId: null, ddbType: 1 },
-  { label: "Stunned", statusId: "Convenient Effect: Stunned", ddbId: 14, levelId: null, ddbType: 1 },
-  { label: "Unconscious", statusId: "Convenient Effect: Unconscious", ddbId: 15, levelId: null, ddbType: 1 },
-];
 
 export function getCondition(conditionName) {
-  return CONDITION_MATRIX.find((condition) => condition.label === conditionName);
+  return DICTIONARY.conditions.find((condition) => condition.label === conditionName);
 }
 
 export async function getActiveConditions(actor) {
-  const conditions = await Promise.all(CONDITION_MATRIX.filter(async (condition) => {
+  const conditions = await Promise.all(DICTIONARY.conditions.filter(async (condition) => {
     const conditionApplied = await game.dfreds.effectInterface.hasEffectApplied(condition.label, actor.uuid);
     return conditionApplied;
   }));
@@ -46,20 +25,22 @@ async function effectAppliedAndActive(condition, actor) {
 }
 
 export async function getActorConditionStates(actor, ddb, keepLocal = false) {
-  const conditions = await Promise.all(CONDITION_MATRIX.map(async (condition) => {
-    const conditionApplied = await effectAppliedAndActive(condition, actor);
-    const ddbCondition = ddb.character.conditions.some((conditionState) =>
-      conditionState.id === condition.ddbId
-      && conditionState.level === condition.levelId
-    );
-    // eslint-disable-next-line require-atomic-updates
-    condition.ddbCondition = ddbCondition;
-    // eslint-disable-next-line require-atomic-updates
-    condition.applied = conditionApplied;
-    // eslint-disable-next-line require-atomic-updates
-    condition.needsUpdate = (ddbCondition && !conditionApplied) || (!ddbCondition && conditionApplied && !keepLocal);
-    return condition;
-  }));
+  const conditions = await Promise.all(DICTIONARY.conditions
+    .filter((condition) => Number.isInteger(condition.ddbId)) // only ddb conditions
+    .map(async (condition) => {
+      const conditionApplied = await effectAppliedAndActive(condition, actor);
+      const ddbCondition = ddb.character.conditions.some((conditionState) =>
+        conditionState.id === condition.ddbId
+        && conditionState.level === condition.levelId
+      );
+      // eslint-disable-next-line require-atomic-updates
+      condition.ddbCondition = ddbCondition;
+      // eslint-disable-next-line require-atomic-updates
+      condition.applied = conditionApplied;
+      // eslint-disable-next-line require-atomic-updates
+      condition.needsUpdate = (ddbCondition && !conditionApplied) || (!ddbCondition && conditionApplied && !keepLocal);
+      return condition;
+    }));
   return conditions;
 }
 
