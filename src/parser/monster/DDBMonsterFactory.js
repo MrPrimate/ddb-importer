25ccb3d4101b0b8ca3993386e3108df42a10a4eb
Jack Holloway <jack@mrprimate.co.uk>
2022-12-26 22:13:36 +0000

    Move to a Monster Factory

---
+++ src/parser/monster/DDBMonsterFactory.js
@@ -0,0 +1,44 @@
+import logger from "../../logger.js";
+import DDBMonster from "./DDBMonster.js";
+
+
+export default class DDBMonsterFactory {
+
+  constructor (ddbData, extra = false) {
+    this.monsterData = ddbData;
+    this.extra = extra;
+    this.npcs = [];
+  }
+
+  async parse() {
+    let foundryActors = [];
+    let failedMonsterNames = [];
+
+    const useItemAC = game.settings.get("ddb-importer", "munching-policy-monster-use-item-ac");
+    const legacyName = game.settings.get("ddb-importer", "munching-policy-legacy-postfix");
+    const addMonsterEffects = game.settings.get("ddb-importer", "munching-policy-add-monster-effects");
+
+    for (const monster of this.monsterData) {
+      try {
+        logger.debug(`Attempting to parse ${monster.name}`);
+        const ddbMonster = new DDBMonster(monster, { extra: this.extra, useItemAC, legacyName, addMonsterEffects });
+        // eslint-disable-next-line no-await-in-loop
+        await ddbMonster.parse();
+        foundryActors.push(duplicate(ddbMonster.npc));
+      } catch (err) {
+        logger.error(`Failed parsing ${monster.name}`);
+        logger.error(err);
+        logger.error(err.stack);
+        failedMonsterNames.push(monster.name);
+      }
+    }
+
+    const result = {
+      actors: await Promise.all(foundryActors),
+      failedMonsterNames: failedMonsterNames,
+    };
+
+    this.npcs = result;
+    return result;
+  }
+}
