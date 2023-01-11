7bc3cbf7d00d176323a4adc1464edf0399100ab3
Jack Holloway <jack@mrprimate.co.uk>
2023-01-11 17:37:53 +0000

    Hunters Mark Macro update

--- macros/spells/huntersMark.js
+++ macros/spells/huntersMark.js
@@ -1,13 +1,60 @@
-let actor = args[0].actor;
-if (actor.flags?.dae?.onUpdateTarget && args[0].hitTargets.length > 0) {
-  const isMarked = actor.flags.dae.onUpdateTarget.find(flag =>
-    flag.flagName === "Hunter's Mark" && flag.sourceTokenUuid === args[0].hitTargetUuids[0]
-  );
-  if (isMarked) {
-    const damageType = args[0].item.system.damage.parts[0][1];
-    const diceMult = args[0].isCritical ? 2: 1;
-    return {damageRoll: `${diceMult}d6[${damageType}]`, flavor: "Hunters Mark Damage"};
+try {
+  const act = actor ?? args[0].actor;
+  if (args[0].macroPass === "DamageBonus") {
+    if (act.flags?.dae?.onUpdateTarget && args[0].hitTargets.length > 0) {
+      const isMarked = act.flags.dae.onUpdateTarget.find(
+        (flag) =>
+          flag.flagName === "Hunter's Mark" &&
+          flag.sourceTokenUuid === args[0].hitTargetUuids[0]
+      );
+      if (isMarked) {
+        let damageType = args[0].item.system.damage.parts[0][1];
+        const diceMult = args[0].isCritical ? 2 : 1;
+        return {
+          damageRoll: `${diceMult}d6[${damageType}]`,
+          flavor: "Hunter's Mark Damage",
+        };
+      }
+    }
+    return {};
+  } else if (args[0].macroPass === "preItemRoll") {
+    // check if we are already marking and if the marked target is dead.
+    const markedTarget = act.flags.dae.onUpdateTarget.find(
+      (flag) => flag.flagName === "Hunter's Mark"
+    )?.sourceTokenUuid;
+    if (markedTarget) {
+      const target = await fromUuid(markedTarget);
+      if (!target || target.actor.system.attributes.hp.value <= 0) {
+        //marked target is dead or removed so don't consume a resource
+        const currentDuration = duplicate(
+          act.effects.find(
+            (ef) => ef.label === game.i18n.localize("midi-qol.Concentrating")
+          ).duration
+        );
+        const useHookId = Hooks.on(
+          "dnd5e.preUseItem",
+          (hookItem, config, options) => {
+            if (hookItem !== item) return;
+            options.configureDialog = false;
+            config.consumeSpellLevel = false;
+            Hooks.off("dnd5e.preUseItem", useHookId);
+          }
+        );
+        const effectHookId = Hooks.on(
+          "preCreateActiveEffect",
+          (effect, data, options, user) => {
+            if (effect.label === game.i18n.localize("midi-qol.Concentrating")) {
+              effect.updateSource({ duration: currentDuration });
+              Hooks.off("dnd5e.preCreateActiveEffect", effectHookId);
+            }
+            return true;
+          }
+        );
+      }
+    }
+    return true;
   }
+} catch (err) {
+  console.error(`${args[0].itemData.name} - Hunter's Mark`, err);
+  return {};
 }
-
-return {};
