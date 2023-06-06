af0806ee32f667feae7e0a56c872110e5b4ae822
Jack Holloway <jack@mrprimate.co.uk>
2023-06-06 21:09:59 +0100

    stoneskill effect improvements

--- src/effects/spells/stoneskin.js
+++ src/effects/spells/stoneskin.js
@@ -3,8 +3,8 @@ import { baseSpellEffect } from "../specialSpells.js";
 export function stoneskinEffect(document) {
   let effect = baseSpellEffect(document, document.name);
   effect.changes.push({
-    key: "system.traits.dr.value",
-    value: "physical",
+    key: "system.traits.dr.custom",
+    value: "Non-Magical Physical",
     mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
     priority: 0,
   });
