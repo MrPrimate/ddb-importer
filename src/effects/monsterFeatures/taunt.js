59190ad08721c67f80db223ab1a560af973d8b6c
Jack Holloway <jack@mrprimate.co.uk>
2023-06-11 10:00:36 +0100

    Missing taunt file

---
+++ src/effects/monsterFeatures/taunt.js
@@ -0,0 +1,21 @@
+
+import { baseItemEffect } from "../effects.js";
+
+export function generateTauntEffect(document) {
+  let effect = baseItemEffect(document, document.name);
+  effect.changes.push(
+    {
+      key: "flags.midi-qol.disadvantage.all",
+      mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+      value: "1",
+      priority: "20",
+    },
+  );
+
+  effect.duration.rounds = 2;
+  effect.duration.seconds = 12;
+  effect.flags.dae.specialDuration = ["turnStart"];
+
+  document.effects.push(effect);
+  return document;
+}
