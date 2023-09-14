df610a903757ccb79126c558d0364d47fe6afdb1
Jack Holloway <jack@mrprimate.co.uk>
2023-09-14 19:06:35 +0100

    Remove dancing lights creation from Dancing lights spell

--- src/effects/spells/dancingLights.js
+++ src/effects/spells/dancingLights.js
@@ -1,11 +1,11 @@
 import { loadMacroFile, generateItemMacroFlag } from "../macros.js";
-import { createJB2aActors, checkJB2a } from "../helpers.js";
+import { checkJB2a } from "../helpers.js";
 import { effectModules } from "../effects.js";
 
 export async function dancingLightsEffect(document) {
   if (!effectModules().warpgateInstalled) return document;
   if (!checkJB2a(true, true, false)) return document;
-  await createJB2aActors("Dancing Lights", "Dancing light");
+  // await createJB2aActors("Dancing Lights", "Dancing light");
 
   const itemMacroText = await loadMacroFile("spell", "dancingLights.js");
   document = generateItemMacroFlag(document, itemMacroText);
