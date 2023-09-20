aa4c7e7a0f2315a7a751e60f421fb8a7f7c986ca
Jack Holloway <jack@mrprimate.co.uk>
2023-09-20 21:22:22 +0100

    Support dynamically sized characters

--- src/parser/character/size.js
+++ src/parser/character/size.js
@@ -1,9 +1,13 @@
 import DICTIONARY from "../../dictionary.js";
+import DDBHelper from "../../lib/DDBHelper.js";
 import DDBCharacter from "../DDBCharacter.js";
 
 DDBCharacter.prototype._generateSize = function _generateSize() {
-  const size = DICTIONARY.character.actorSizes.find(
-    (size) => size.name === this.source.ddb.character.race.size || size.id === this.source.ddb.character.race.sizeId
+  const sizeMods = DDBHelper.filterModifiers(this.source.ddb.character.modifiers.race, "size");
+  const size = DICTIONARY.character.actorSizes.find((size) =>
+    size.name === this.source.ddb.character.race.size
+    || size.id === this.source.ddb.character.race.sizeId
+    || (this.source.ddb.character.race.sizeId === 10 && sizeMods.some((mod) => mod.subType === size.name.toLowerCase()))
   );
   this.raw.character.system.traits.size = size ? size.value : "med";
 };
