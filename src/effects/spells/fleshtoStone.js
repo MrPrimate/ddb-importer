d09a7f177cb6eb61d7e6cface8e8724758424817
Jack Holloway <jack@mrprimate.co.uk>
2023-09-29 20:38:06 +0100

    Flesh to stone fix

--- src/effects/spells/fleshtoStone.js
+++ src/effects/spells/fleshtoStone.js
@@ -3,7 +3,7 @@ import DDBMacros from "../macros.js";
 
 export async function fleshtoStoneEffect(document) {
   let effect = baseSpellEffect(document, document.name);
-  effect.changes.push(generateStatusEffectChange("Restrained"));
+  effect.changes.push(generateStatusEffectChange("Restrained", 20, true));
   await DDBMacros.setItemMacroFlag(document, "spell", "fleshtoStone.js");
   effect.flags.dae.macroRepeat = "endEveryTurn";
   effect.changes.push(DDBMacros.generateMacroChange({ macroType: "spell", macroName: "fleshtoStone.js" }));
