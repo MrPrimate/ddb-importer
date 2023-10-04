5c2f8ef8c5cb533b666661ea563400ca59a6b805
Jack Holloway <jack@mrprimate.co.uk>
2023-10-04 20:00:00 +0100

    Improvements to size on pcs with variable size

--- src/parser/character/size.js
+++ src/parser/character/size.js
@@ -4,10 +4,11 @@ import DDBCharacter from "../DDBCharacter.js";
 
 DDBCharacter.prototype._generateSize = function _generateSize() {
   const sizeMods = DDBHelper.filterModifiers(this.source.ddb.character.modifiers.race, "size");
-  const size = DICTIONARY.character.actorSizes.find((size) =>
-    size.name === this.source.ddb.character.race.size
-    || size.id === this.source.ddb.character.race.sizeId
-    || (this.source.ddb.character.race.sizeId === 10 && sizeMods.some((mod) => mod.subType === size.name.toLowerCase()))
-  );
+  const size = (sizeMods.length > 0)
+    ? DICTIONARY.character.actorSizes.find((size) => sizeMods.some((mod) => mod.subType === size.name.toLowerCase()))
+    : DICTIONARY.character.actorSizes.find((size) =>
+      size.name === this.source.ddb.character.race.size
+      || size.id === this.source.ddb.character.race.sizeId);
+
   this.raw.character.system.traits.size = size ? size.value : "med";
 };
