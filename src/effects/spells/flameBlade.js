413662be3b5faa76f9ba353880327c0aab87fd5f
Jack Holloway <jack@mrprimate.co.uk>
2023-11-10 22:32:18 +0000

    Effect: Flameblade ATL

--- src/effects/spells/flameBlade.js
+++ src/effects/spells/flameBlade.js
@@ -3,7 +3,24 @@ import DDBMacros from "../macros.js";
 
 export async function flameBladeEffect(document) {
   let effect = baseSpellEffect(document, document.name);
-  effect.changes.push(DDBMacros.generateMacroChange({ macroValues: "@spellLevel", macroType: "spell", macroName: "flameBlade.js" }));
+  effect.changes.push(
+    DDBMacros.generateMacroChange({ macroValues: "@spellLevel", macroType: "spell", macroName: "flameBlade.js" })
+  );
+
+  effect.changes.push(
+    {
+      key: "ATL.light.dim",
+      mode: CONST.ACTIVE_EFFECT_MODES.UPGRADE,
+      value: "20",
+      priority: 20,
+    },
+    {
+      key: "ATL.light.bright",
+      mode: CONST.ACTIVE_EFFECT_MODES.UPGRADE,
+      value: "10",
+      priority: 20,
+    }
+  );
   setProperty(effect, "flags.dae.selfTarget", true);
   setProperty(effect, "flags.dae.selfTargetAlways", true);
   document.system.actionType = "other";
