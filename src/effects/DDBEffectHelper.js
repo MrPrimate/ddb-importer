9ffb7a14250990525108a5195d52ad7e99f6c16e
Jack Holloway <jack@mrprimate.co.uk>
2024-02-01 22:08:16 +0000

    export addStatusEffectChange

--- src/effects/DDBEffectHelper.js
+++ src/effects/DDBEffectHelper.js
@@ -28,6 +28,8 @@ export default class DDBEffectHelper {
 
   static generateStatusEffectChange = generateStatusEffectChange;
 
+  static addStatusEffectChange = this.addStatusEffectChange;
+
   /**
    * Generates and applies DDBI effects to a document.
    *
