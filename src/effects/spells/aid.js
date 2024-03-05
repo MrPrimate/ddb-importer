93f7bcaaadd6f7b297599280edc68956c0d26b34
Jack Holloway <jack@mrprimate.co.uk>
2024-03-05 21:34:55 +0000

    Aid corrections

--- src/effects/spells/aid.js
+++ src/effects/spells/aid.js
@@ -5,17 +5,9 @@ import { baseSpellEffect } from "../specialSpells.js";
 export async function aidEffect(document) {
   let effect = baseSpellEffect(document, document.name);
 
-  if (effectModules().daeInstalled) {
+  if (effectModules().midiQolInstalled) {
     effect.changes.push({
-      key: "system.attributes.hp.tempmax",
-      value: "5 * (@spellLevel - 1)",
-      mode: CONST.ACTIVE_EFFECT_MODES.ADD,
-      priority: 20,
-    });
-  } else if (effectModules().midiQolInstalled) {
-
-    effect.changes.push({
-      key: "system.attributes.hp.tempmax",
+      key: "system.attributes.hp.bonuses.overall",
       value: "5 * (@spellLevel - 1)",
       mode: CONST.ACTIVE_EFFECT_MODES.ADD,
       priority: 20,
@@ -23,6 +15,13 @@ export async function aidEffect(document) {
     document.system.damage = { parts: [], versatile: "", value: "" };
     await DDBMacros.setItemMacroFlag(document, "spell", "aid.js");
     effect.changes.push(DDBMacros.generateMacroChange({ macroValues: "@spellLevel", macroType: "spell", macroName: "aid.js", priority: 0 }));
+  } else if (effectModules().daeInstalled) {
+    effect.changes.push({
+      key: "system.attributes.hp.bonuses.overall",
+      value: "5 * (@spellLevel - 1)",
+      mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+      priority: 20,
+    });
   }
   document.effects.push(effect);
   return document;
