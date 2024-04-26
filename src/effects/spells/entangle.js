9ebdcab2a0039ba2a36ae1776788abe69c221874
Jack Holloway <jack@mrprimate.co.uk>
2024-04-26 20:54:24 +0100

    Entangle Effect

--- src/effects/spells/entangle.js
+++ src/effects/spells/entangle.js
@@ -5,16 +5,25 @@ export function entangleEffect(document) {
   let effect = baseSpellEffect(document, `${document.name} - Restrained`);
   addStatusEffectChange(effect, "Restrained", 20, true);
 
-  // not implemented as the target can choose to escape with it's action
-  // effect.changes.push(
-  //   {
-  //     key: "flags.midi-qol.OverTime",
-  //     mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
-  //     value:
-  //       `label=${document.name} (Start of Turn),turn=start, saveAbility=str, saveDC=@attributes.spelldc, rollType=save, saveMagic=true, saveRemove=true`,
-  //     priority: "20",
-  //   },
-  // );
+  effect.changes.push(
+    {
+      key: "flags.midi-qol.OverTime",
+      mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+      value: "You can take an action to break free by rolling a Strength Ability Check",
+      priority: "20",
+    },
+    {
+      key: "flags.midi-qol.OverTime",
+      mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+      value: `turn=end, rollType=check, actionSave=true, saveAbility=str, saveDC=@attributes.spelldc, label=Restrained by ${document.name}`,
+      priority: "20",
+    },
+  );
+
+  foundry.utils.setProperty(effect, "duration.seconds", 60);
+  foundry.utils.setProperty(effect, "duration.rounds", 10);
+  foundry.utils.setProperty(effect, "flags.dae.stackable", "noneName");
+
   document.effects.push(effect);
 
   return document;
