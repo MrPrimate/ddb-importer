a6d615f252f34a4de1906a7b0efac72e83c654da
Jack Holloway <jack@mrprimate.co.uk>
2024-05-28 22:17:02 +0100

    Ordinal generator

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -544,6 +544,21 @@ const utils = {
     }
     return true;
   },
+
+  ordinalSuffixOf(i) {
+    let j = i % 10,
+      k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  },
 };
 
 export default utils;
