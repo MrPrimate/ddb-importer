a5743fe7fae511445ff803af2bc0ced464a768d3
Themis <idonotwantspams@gmail.com>
2024-10-07 21:51:47 +0100

    Update sleep.js

--- macros/spells/sleep.js
+++ macros/spells/sleep.js
@@ -1,12 +1,15 @@
 // based on @ccjmk and @crymic macro for sleep. Gets targets and ignores those who are immune to sleep.
-// uses convinient effects
 // Midi-qol "On Use"
 
-const sleepHp = await args[0].damageTotal;
+const isVersion12 = game.version > 12;
+const getEffects = isVersion12 ? "appliedEffects" : "effects";
+const labelName = isVersion12 ? "name" : "label";
+const localizedEffectName = game.i18n.translations["ddb-importer"]["item-type"].specificSpells.Sleep;
+const sleepHp = workflow.damageTotal;
 const condition = "Unconscious";
 console.log(`Sleep Spell => Available HP Pool [${sleepHp}] points`);
-const targets = await args[0].targets
-  .filter((i) => i.actor.system.attributes.hp.value != 0 && !i.actor.effects.find((x) => (x.name ?? x.label) === condition))
+const targets = workflow.targets
+  .filter((i) => i.actor.system.attributes.hp.value != 0 && !i.actor[getEffects].find((x) => x[labelName] === condition))
   .sort((a, b) => (canvas.tokens.get(a.id).actor.system.attributes.hp.value < canvas.tokens.get(b.id).actor.system.attributes.hp.value ? -1 : 1));
 let remainingSleepHp = sleepHp;
 let sleepTarget = [];
@@ -15,8 +18,8 @@ for (let target of targets) {
   const findTarget = await canvas.tokens.get(target.id);
   const targetRaceOrType = DDBImporter?.EffectHelper.getRaceOrType(findTarget.actor);
   const immuneType = ["undead", "construct", "elf", "half-elf"].some((race) => targetRaceOrType.includes(race));
-  const immuneCI = findTarget.actor.system.traits.ci.custom.includes("Sleep");
-  const sleeping = findTarget.actor.effects.find((i) => (i.name ?? i.label) === condition);
+  const immuneCI = findTarget.actor.system.traits.ci.custom.toLowerCase().includes(localizedEffectName);
+  const sleeping = findTarget.actor[getEffects].find((i) => i[labelName] === condition);
   const targetHpValue = findTarget.actor.system.attributes.hp.value;
   const targetImg = target?.texture?.src;
   if ((immuneType) || (immuneCI) || (sleeping)) {
@@ -28,10 +31,9 @@ for (let target of targets) {
     sleepTarget.push(`<div class="midi-qol-flex-container"><div>Slept</div><div class="midi-qol-target-npc midi-qol-target-name" id="${findTarget.id}"> ${findTarget.name}</div><div><img src="${targetImg}" width="30" height="30" style="border:0px"></div></div>`);
     const gameRound = game.combat ? game.combat.round : 0;
     const effectData = {
-      label: "Sleep Spell",
-      name: "Sleep Spell",
+      [labelName]: "Sleep Spell",
       img: "icons/svg/sleep.svg",
-      origin: args[0].uuid,
+      origin: rolledItem.uuid,
       disabled: false,
       duration: { rounds: 10, seconds: 60, startRound: gameRound, startTime: game.time.worldTime },
       flags: { dae: { specialDuration: ["isDamaged"] } },
@@ -46,9 +48,9 @@ for (let target of targets) {
 }
 await DDBImporter?.EffectHelper.wait(500);
 const sleptResults = `<div><div class="midi-qol-nobox">${sleepTarget.join('')}</div></div>`;
-const chatMessage = game.messages.get(args[0].itemCardId);
+const chatMessage = game.messages.get(workflow.itemCardId);
 let content = foundry.utils.duplicate(chatMessage.content);
 const searchString = /<div class="midi-qol-hits-display">[\s\S]*<div class="end-midi-qol-hits-display">/g;
 const replaceString = `<div class="midi-qol-hits-display"><div class="end-midi-qol-hits-display">${sleptResults}`;
-content = await content.replace(searchString, replaceString);
+content = content.replace(searchString, replaceString);
 await chatMessage.update({ content: content });
