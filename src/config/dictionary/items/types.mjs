d26f465915de0e25a23559f201386862e129b65d
Jack Holloway <jack@mrprimate.co.uk>
2025-01-02 22:38:58 +0000

    TASHAS Tattoos

--- src/config/dictionary/items/types.mjs
+++ src/config/dictionary/items/types.mjs
@@ -1,6 +1,19 @@
 export const ITEM_TYPES = {
-  full: ["weapon", "equipment", "consumable", "tool", "loot", "class", "subclass", "background", "spell", "feat", "container"],
-  equipment: ["equipment", "consumable", "tool", "loot", "container"],
-  inventory: ["equipment", "consumable", "tool", "loot", "container", "weapon"],
+  full: [
+    "weapon",
+    "equipment",
+    "consumable",
+    "tool",
+    "loot",
+    "class",
+    "subclass",
+    "background",
+    "spell",
+    "feat",
+    "container",
+    "dnd-tashas-cauldron.tattoo",
+  ],
+  equipment: ["equipment", "consumable", "tool", "loot", "container", "dnd-tashas-cauldron.tattoo"],
+  inventory: ["equipment", "consumable", "tool", "loot", "container", "weapon", "dnd-tashas-cauldron.tattoo"],
   monster: ["equipment", "consumable", "tool", "loot", "container", "weapon", "feat"],
 };
