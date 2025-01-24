be3f2e1ece0a4981f95c47af42554254a6875047
Jack Holloway <jack@mrprimate.co.uk>
2025-01-24 21:02:22 +0000

    Test Aura changes

--- src/effects/auras/_module.mjs
+++ src/effects/auras/_module.mjs
@@ -1,4 +1,4 @@
-export { default as test } from "./test.mjs";
+export { default as Test } from "./Test.mjs";
 export { default as ConditionOnEntry } from "./ConditionOnEntry.mjs";
 export { default as DamageOnEntry } from "./DamageOnEntry.mjs";
 export { default as AuraOnly } from "./AuraOnly.mjs";
