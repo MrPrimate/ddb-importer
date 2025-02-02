406fad4990e0d62d812b6290b602d90a4c6eb34a
Jack Holloway <jack@mrprimate.co.uk>
2025-02-02 15:03:32 +0000

    Entangle fixes

--- src/parser/enrichers/spell/Entangle.mjs
+++ src/parser/enrichers/spell/Entangle.mjs
@@ -4,24 +4,46 @@ import DDBEnricherData from "../data/DDBEnricherData.mjs";
 export default class Entangle extends DDBEnricherData {
 
   get effects() {
+    if (this.is2014) {
+      return [
+        {
+          noCreate: true,
+          midiOnly: true,
+          midiChanges: [
+            DDBEnricherData.ChangeHelper.customChange(
+              "You can take an action to break free by rolling a Strength Ability Check",
+              20,
+              "flags.midi-qol.OverTime",
+            ),
+            DDBEnricherData.ChangeHelper.customChange(
+              `turn=end, rollType=check, actionSave=true, saveAbility=str, saveDC=@attributes.spelldc, label=Restrained by ${this.data.name}`,
+              20,
+              "flags.midi-qol.OverTime",
+            ),
+          ],
+        },
+      ];
+    }
+
     return [
       {
         noCreate: true,
         midiOnly: true,
         midiChanges: [
           DDBEnricherData.ChangeHelper.customChange(
-            "You can take an action to break free by rolling a Strength Ability Check",
+            "You can take an action to break free by rolling a Athletics (Strength) Ability Check",
             20,
             "flags.midi-qol.OverTime",
           ),
           DDBEnricherData.ChangeHelper.customChange(
-            `turn=end, rollType=check, actionSave=true, saveAbility=str, saveDC=@attributes.spelldc, label=Restrained by ${this.data.name}`,
+            `turn=end, rollType=check, actionSave=true, saveAbility=ath, saveDC=@attributes.spelldc, label=Restrained by ${this.data.name}`,
             20,
             "flags.midi-qol.OverTime",
           ),
         ],
       },
     ];
+
   }
 
 }
