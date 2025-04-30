3e7f0ec867ea7fbbe688a22eaf0b3005ef525c02
Jack Holloway <jack@mrprimate.co.uk>
2025-04-30 21:55:22 +0100

    spellmonsters

--- src/parser/lib/DDBDescriptions.mjs
+++ src/parser/lib/DDBDescriptions.mjs
@@ -511,7 +511,7 @@ export default class DDBDescriptions {
     // Regular expression to match commas not inside brackets
     const regex = /,(?![^(]*\))/g;
     const result = str.split(regex);
-    return result.map((item) => item.trim());
+    return result.map((item) => item.replaceAll("*", "").trim());
   }
 
   static parseOutMonsterSpells(text) {
