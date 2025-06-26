b35473df1c237529380f3c02c1de7674411b230f
Jack Holloway <jack@mrprimate.co.uk>
2025-06-26 08:43:30 +0100

    Passwithout trace

--- src/parser/enrichers/spell/PassWithoutTrace.mjs
+++ src/parser/enrichers/spell/PassWithoutTrace.mjs
@@ -9,6 +9,32 @@ export default class PassWithoutTrace extends DDBEnricherData {
         changes: [
           DDBEnricherData.ChangeHelper.signedAddChange("10", 20, "system.skills.ste.bonuses.check"),
         ],
+        data: {
+          flags: {
+            "dae.stackable": "noneNameOnly",
+            ActiveAuras: {
+              aura: "Allies",
+              radius: "30",
+              isAura: true,
+              inactive: false,
+              hidden: false,
+              displayTemp: true,
+            },
+          },
+        },
+        auraeffects: {
+          applyToSelf: true,
+          bestFormula: "",
+          canStack: false,
+          collisionTypes: ["move"],
+          combatOnly: false,
+          disableOnHidden: true,
+          distanceFormula: "30",
+          disposition: 1,
+          evaluatePreApply: true,
+          overrideName: "",
+          script: "",
+        },
       },
     ];
   }
