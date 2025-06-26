2372c2c0e6cc112f3134b39b0d8c426fc4001386
Jack Holloway <jack@mrprimate.co.uk>
2025-06-26 21:05:42 +0100

    Elemental adept

--- src/parser/enrichers/feat/ElementalAdept.mjs
+++ src/parser/enrichers/feat/ElementalAdept.mjs
@@ -4,34 +4,8 @@ import DDBEnricherData from "../data/DDBEnricherData.mjs";
 
 export default class ElementalAdept extends DDBEnricherData {
 
-  // get effects() {
-  //   const activeType = this.ddbParser?._chosen.find((a) =>
-  //     utils.nameString(a.label).startsWith("Boon of Energy Resistance"),
-  //   )?.label ?? "";
-
-  //   const types = ["Acid", "Cold", "Fire", "Lightning", "Necrotic", "Poison", "Psychic", "Radiant", "Thunder"];
-  //   const multiple = [];
-  //   types.forEach((type) => {
-  //     multiple.push({
-  //       name: `Boon of Energy Resistance: ${type}`,
-  //       options: {
-  //         transfer: true,
-  //         disabled: !activeType.includes(type),
-  //       },
-  //       changes: [
-  //         DDBEnricherData.ChangeHelper.unsignedAddChange(type.toLowerCase(), 20, "system.traits.dr.value"),
-  //       ],
-  //     });
-  //   });
-
-  //   return multiple;
-  // }
-
   get override() {
     if (this.is2014) return null;
-    console.warn("BoonOfEnergyResistance overrides not implemented", {
-      this: this,
-    });
 
     const types = ["Acid", "Cold", "Fire", "Lightning", "Necrotic", "Poison", "Psychic", "Radiant", "Thunder"];
 
