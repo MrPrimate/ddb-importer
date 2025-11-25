c02f9d0f8e2bc447782fb33c676fd3e8e6c492fd
Jack Holloway <jack@mrprimate.co.uk>
2025-11-25 21:04:09 +0000

    Language fixes

--- src/hooks/ready/addDDBConfig.js
+++ src/hooks/ready/addDDBConfig.js
@@ -3,29 +3,63 @@ import { DDBSources, utils } from "../../lib/_module.mjs";
 import { DDBRuleJournalFactory } from "../../parser/lib/_module.mjs";
 import SpellListFactory from "../../parser/spells/SpellListFactory.mjs";
 
+function filterLanguages(languages) {
+  const result = new Set();
+
+  const systemLanguageValues = new Set();
+
+  const processCategory = (key, data, group) => {
+
+    if (!data.children) {
+      systemLanguageValues.add(key);
+    }
+
+    if (data.children && (data.selectable !== false)) {
+      systemLanguageValues.add(key);
+      group ??= { label: data.label, children: [] };
+      Object.entries(data.children).forEach(([k, d]) => processCategory(k, d, group));
+    } else if (data.children) {
+      Object.entries(data.children).forEach(([k, d]) => processCategory(k, d));
+    }
+  };
+
+  for (const [key, data] of Object.entries(CONFIG.DND5E.languages)) {
+    if (data.children) Object.entries(data.children).forEach(([k, d]) => processCategory(k, d));
+    else processCategory(key, data);
+  }
+
+  for (const lang of languages) {
+    const name = utils.nameString(lang.name);
+    if (["All", "Telepathy"].includes(name)) continue;
+    if (DICTIONARY.actor.languages.some((l) => l.name === name)) continue;
+    const stub = utils.normalizeString(name);
+    if (systemLanguageValues.has(stub)) continue;
+
+    systemLanguageValues.add(stub);
+    result.add({ name, value: stub });
+  };
+  return Array.from(result);
+
+}
+
 function addLanguages() {
   if (!game.settings.get(SETTINGS.MODULE_ID, "add-ddb-languages")) return;
   const ddbRaw = foundry.utils.getProperty(CONFIG, "DDB.languages");
   if (!ddbRaw) return;
 
-  const ddbFiltered = [...new Set(ddbRaw
-    .map((lang) => utils.nameString(lang.name))
-    .filter((lang) =>
-      !DICTIONARY.actor.languages.some((l) => l.name === lang)
-      && !["All"].includes(lang),
-    ))];
+  const ddbFiltered = filterLanguages(ddbRaw);
 
   CONFIG.DND5E.languages.ddb = {
     label: "D&D Beyond Rare Languages",
     children: {
     },
+    selectable: false,
   };
   ddbFiltered.forEach((lang) => {
-    const stub = utils.normalizeString(lang);
-    CONFIG.DND5E.languages.ddb.children[stub] = lang;
+    CONFIG.DND5E.languages.ddb.children[lang.value] = lang.name;
     DICTIONARY.actor.languages.push({
-      name: lang,
-      value: stub,
+      name: lang.name,
+      value: lang.value,
       advancement: "ddb",
     });
   });
