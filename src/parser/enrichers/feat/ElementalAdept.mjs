c88ddb77b44a9d5ce89769dc51b679ca0c70deda
Jack Holloway <jack@mrprimate.co.uk>
2025-12-24 19:34:06 +0000

    Elemental adept

--- src/parser/enrichers/feat/ElementalAdept.mjs
+++ src/parser/enrichers/feat/ElementalAdept.mjs
@@ -6,13 +6,17 @@ export default class ElementalAdept extends DDBEnricherData {
   get override() {
     if (this.is2014) return null;
 
-    const types = ["Acid", "Cold", "Fire", "Lightning", "Necrotic", "Poison", "Psychic", "Radiant", "Thunder"];
+    const types = [
+      "Acid",
+      "Cold",
+      "Fire",
+      "Lightning",
+      "Thunder",
+    ];
 
-    const activeType = this.ddbParser.isMuncher
-      ? null
-      : this.ddbParser._chosen?.find((a) =>
-        types.includes(a.label),
-      )?.label;
+    const activeType = this.ddbParser._chosen?.find((a) =>
+      types.includes(a.label),
+    )?.label;
 
     const name = activeType
       ? `${this.ddbParser?.data?.name ?? ""} (${activeType})`
