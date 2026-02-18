7baa412c43a4a7172be1a65d959e4fdc2f935842
Jack Holloway <jack@mrprimate.co.uk>
2026-02-18 21:00:46 +0000

    Ascendant Monk

--- src/parser/enrichers/class/monk/AspectOfTheWyrm.mjs
+++ src/parser/enrichers/class/monk/AspectOfTheWyrm.mjs
@@ -20,6 +20,8 @@ export default class AspectOfTheWyrm extends DDBEnricherData {
   get activity() {
     return {
       name: "Resistance",
+      noConsumeTargets: true,
+      addItemConsume: true,
       activationType: "bonus",
       targetType: "creature",
       data: {
@@ -32,12 +34,52 @@ export default class AspectOfTheWyrm extends DDBEnricherData {
    * @returns {DDBAdditionalActivity[]}
    */
   get additionalActivities() {
+    const spend = this.is2014 ? "Ki" : "Monk's Focus";
     return [
       {
         action: {
           name: "Aspect of the Wyrm: Frightful Presence",
           type: "class",
           rename: ["Frightful Presence"],
+        },
+        overrides: {
+          noConsumeTargets: true,
+          addItemConsume: true,
+        },
+      },
+      {
+        constructor: {
+          name: `Spend ${spend} to Restore Use`,
+          type: "utility",
+        },
+        build: {
+          generateConsumption: true,
+          generateTarget: true,
+          generateActivation: true,
+          generateUtility: true,
+          noEffects: true,
+          noeffect: true,
+          activationOverride: {
+            type: "none",
+            value: null,
+            condition: "",
+          },
+          consumptionOverride: {
+            targets: [
+              {
+                type: "itemUses",
+                target: "",
+                value: -1,
+                scaling: { mode: "", formula: "" },
+              },
+              {
+                type: "itemUses",
+                value: "3",
+                target: spend,
+                scaling: { allowed: false, max: "" },
+              },
+            ],
+          },
         },
       },
     ];
@@ -51,8 +93,9 @@ export default class AspectOfTheWyrm extends DDBEnricherData {
    * @returns {DDBEffectHint[]}
    */
   get effects() {
-    return ["Acid", "Cold", "Fire", "Lightning", "Poison"].map((damageType) => ({
+    const resistanceEffects = ["Acid", "Cold", "Fire", "Lightning", "Poison"].map((damageType) => ({
       name: `Aspect of the Wyrm: Resistance (${damageType})`,
+      activityMatch: "Resistance",
       changes: [
         DDBEnricherData.ChangeHelper.damageResistanceChange(damageType),
       ],
@@ -60,15 +103,33 @@ export default class AspectOfTheWyrm extends DDBEnricherData {
         durationSeconds: 600,
       },
     }));
+
+    const featEffect = [
+      {
+        name: "Aspect of the Wyrm: Frightful Presence",
+        activitiesMatch: ["Aspect of the Wyrm: Frightful Presence", "Frightful Presence"],
+        statuses: ["frightened"],
+        options: {
+          durationSeconds: 60,
+        },
+        midiChanges: [
+          DDBEnricherData.ChangeHelper.customChange(
+            `turn=end, savingThrow=true, saveAbility=wis, saveDC=@attributes.spell.dc, label=Frightened by ${this.data.name}`,
+            20,
+            "flags.midi-qol.OverTime",
+          ),
+        ],
+      },
+    ];
+    return [...resistanceEffects, ...featEffect];
   }
 
-  // get override() {
-  //   return {
-  //     replaceActivityUses: true,
-  //     data: {
-  //       "flags.ddbimporter.skipScale": true,
-  //     },
-  //   };
-  // }
-
+  get override() {
+    return {
+      replaceActivityUses: true,
+    //     data: {
+    //       "flags.ddbimporter.skipScale": true,
+    //     },
+    };
+  }
 }
