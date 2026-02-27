6d306d76d7cc88b70b2f72f4da288c287b0583b1
Jack Holloway <jack@mrprimate.co.uk>
2026-02-27 21:44:41 +0000

    Healing

--- src/parser/enrichers/class/warlock/GraspingTentacles.ts
+++ src/parser/enrichers/class/warlock/GraspingTentacles.ts
@@ -2,6 +2,23 @@ import DDBEnricherData from "../../data/DDBEnricherData";
 
 export default class GraspingTentacles extends DDBEnricherData {
 
+  get type() {
+    return "healing";
+  }
+
+  get activity() {
+    return {
+      targetType: "self",
+      activationType: "special",
+      data: {
+        healing: DDBEnricherData.basicDamagePart({
+          customFormula: "@classes.warlock.levels",
+          types: ["temphp"],
+        }),
+      },
+    };
+  }
+
   get override() {
     return {
       forceSpellAdvancement: true,
