a2c26fedcbef0bfa825b87f22c03150e464cd250
Jack Holloway <jack@mrprimate.co.uk>
2026-04-11 17:08:17 +0100

    Deprecation warning

--- macros/monsterFeatures/disassemble.js
+++ macros/monsterFeatures/disassemble.js
@@ -5,7 +5,7 @@ if ( !(active('jb2a_patreon') || active('JB2A_DnD5e')) ) return;
 
 if ( args[ 0 ] === "off" ) {
   const imgs = await game.actors.getName("Skeleton").getTokenImages()
-  await Promise.all(imgs.map(i => loadTexture(i)));
+  await Promise.all(imgs.map(i => foundry.canvas.loadTexture(i)));
   for(let i = 0; i < 12; i++){
     const [spawn] = await warpgate.spawn("Skeleton");
     new Sequence().effect().file("jb2a.misty_step.01.blue").atLocation(spawn).play()
