2fcf1b09466adfc9069b5cb22f01fe254e5e59ba
Jack Holloway <jack@mrprimate.co.uk>
2026-04-29 21:29:29 +0100

    Bladesong improvements

--- src/parser/enrichers/class/wizard/Bladesong.ts
+++ src/parser/enrichers/class/wizard/Bladesong.ts
@@ -3,7 +3,7 @@ import DDBEnricherData from "../../data/DDBEnricherData";
 export default class Bladesong extends DDBEnricherData {
 
   get type() {
-    return DDBEnricherData.ACTIVITY_TYPES.UTILITY;
+    return DDBEnricherData.ACTIVITY_TYPES.ENCHANT;
   }
 
   get activity(): IDDBActivityData {
@@ -12,6 +12,10 @@ export default class Bladesong extends DDBEnricherData {
       activationType: "bonus",
       data: {
         name: "Activate Bladesong",
+        restrictions: {
+          type: "weapon",
+          allowMagical: true,
+        },
       },
     };
   }
@@ -19,8 +23,45 @@ export default class Bladesong extends DDBEnricherData {
   get effects(): IDDBEffectHint[] {
     return [
       {
+        type: "enchant",
+        name: "Bladework (Int)",
+        changes: [
+          DDBEnricherData.ChangeHelper.overrideChange(`{} [Bladework]`, 20, "name"),
+          DDBEnricherData.ChangeHelper.unsignedAddChange(`@abilities.int.mod`, 50, "activities[attack].attack.bonus"),
+        ],
         options: {
           durationSeconds: 60,
+        },
+        data: {
+          flags: {
+            ddbimporter: {
+              effectRiders: ["ddbBladeSongEff1"],
+            },
+          },
+        },
+      },
+      {
+        type: "enchant",
+        name: "Bladework (Physical)",
+        changes: [
+          DDBEnricherData.ChangeHelper.overrideChange(`{} [Bladework]`, 20, "name"),
+        ],
+        options: {
+          durationSeconds: 60,
+        },
+        data: {
+          flags: {
+            ddbimporter: {
+              effectRiders: ["ddbBladeSongEff1"],
+            },
+          },
+        },
+      },
+      {
+        name: "Bladesong",
+        options: {
+          durationSeconds: 60,
+          transfer: true,
         },
         changes: [
           DDBEnricherData.ChangeHelper.unsignedAddChange("max(@abilities.int.mod,1)", 20, "system.attributes.ac.bonus"),
@@ -28,6 +69,10 @@ export default class Bladesong extends DDBEnricherData {
           DDBEnricherData.ChangeHelper.unsignedAddChange("10", 20, "system.attributes.movement.walk"),
           DDBEnricherData.ChangeHelper.unsignedAddChange(`${CONFIG.Dice.D20Roll.ADV_MODE.ADVANTAGE}`, 20, "system.skills.acr.roll.mode"),
         ],
+        activitiesMatch: ["Not real"],
+        data: {
+          _id: "ddbBladeSongEff1",
+        },
       },
     ];
   }
