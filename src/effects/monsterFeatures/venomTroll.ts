a42db0d8900617b7b5b26feafd636ab12486badc
Jack Holloway <jack@mrprimate.co.uk>
2026-05-18 15:02:53 +0100

    Venom troll tweak

--- src/effects/monsterFeatures/venomTroll.ts
+++ src/effects/monsterFeatures/venomTroll.ts
@@ -20,8 +20,8 @@ export async function venomTrollEffects(npc) {
       effect.system.changes.push(
         {
           key: "flags.midi-qol.OverTime",
-          type: "custom",
-          value: `turn=start, damageRoll=${item.system.damage.parts[0][0]}, damageType=healing, condition=@attributes.hp.value > 0 && @attributes.hp.value < @attributes.hp.max, rollMode=gmroll, label=${item.name} (Fire or Acid prevents)`,
+          mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+          value: `turn=start, damageRoll=10, damageType=healing, condition=@attributes.hp.value > 0 && @attributes.hp.value < @attributes.hp.max, rollMode=gmroll, label=${item.name} (Fire or Acid prevents)`,
           priority: "20",
         },
       );
