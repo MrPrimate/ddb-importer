07083a573e14bbd164322b2607d2958e415bb995
Jack Holloway <jack@mrprimate.co.uk>
2026-05-28 13:03:09 +0100

    Haste fix

--- src/parser/enrichers/item/PotionOfSpeed.ts
+++ src/parser/enrichers/item/PotionOfSpeed.ts
@@ -5,6 +5,9 @@ export default class PotionOfSpeed extends DDBEnricherData {
   get effects(): IDDBEffectHint[] {
     return [
       {
+        options: {
+          transfer: false,
+        },
         changes: [
           DDBEnricherData.ChangeHelper.signedAddChange("2", 20, "system.attributes.ac.bonus"),
           DDBEnricherData.ChangeHelper.unsignedAddChange(`${CONFIG.Dice.D20Roll.ADV_MODE.ADVANTAGE}`, 20, "system.abilities.dex.save.roll.mode"),
