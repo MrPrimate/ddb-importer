ba0192f4fc4734096bb88c77c7030aa57c837a09
Jack Holloway <jack@mrprimate.co.uk>
2026-06-07 11:04:21 +0100

    Dhampir

--- src/parser/enrichers/trait/dhampir/VampiricBite.ts
+++ src/parser/enrichers/trait/dhampir/VampiricBite.ts
@@ -6,4 +6,58 @@ export default class VampiricBite extends DDBEnricherData {
     return DDBEnricherData.ACTIVITY_TYPES.ATTACK;
   }
 
+  get activity(): IDDBActivityData {
+    if (this.is2014) {
+      return null;
+    }
+    return {
+      noConsumeTargets: true,
+      data: {
+        damage: {
+          includeBase: false,
+          parts: [
+            DDBEnricherData.basicDamagePart({
+              number: 1,
+              denomination: 4,
+              bonus: "@abilities.con.mod",
+              types: ["piercing"],
+            }),
+          ],
+        },
+      },
+    };
+  }
+
+  get additionalActivities(): IDDBAdditionalActivity[] {
+    if (this.is2014) {
+      return [];
+    }
+    return [
+      {
+        init: {
+          name: "Empower Self",
+          type: DDBEnricherData.ACTIVITY_TYPES.HEAL,
+        },
+        overrides: {
+          addItemConsume: true,
+          activationType: "special",
+          data: {
+            healing: DDBEnricherData.basicDamagePart({
+              customFormula: "@scaling",
+              type: "healing",
+            }),
+            consumption: {
+              scaling: {
+                allowed: true,
+                max: "20",
+              },
+              spellSlot: true,
+              targets: [],
+            },
+          },
+        },
+      },
+    ];
+  }
+
 }
